0cce654c9b8d0f287856f326f7d350a561a8bcd8
bradyz <brady.zhou@utexas.edu>
2014-09-07 18:20:17 -0500

    BZ uva stuff

---
+++ uva/208/Vertex208.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Vertex208 {
+  public class Vertex {
+    int label;
+  }
+
+  public class Edge {
+    int from, to;
+  }
+
+  public class AdjacentMatrix {
+    Map<Vertex, Map<Vertex>> Adjacents;
+
+  }
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNext()) {
+      int input = sc.nextInt();
+
+      if (input == 0) {
+        System.out.println();
+      }
+      
+    }
+
+  }
+}
