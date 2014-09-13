ac586c4d3b62673cf9dd107773ebba763a0f5942
bradyz <brady.zhou@utexas.edu>
2014-09-13 10:00:28 -0500

    BZ moving crap out

--- Instabot/ip_refresher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=0
-while [ $COUNTER -lt 2 ];do
-	./sleep_run.sh & ./tor_setup.sh
-	(( COUNTER++ ))
-done	
-	
--- Instabot/sleep_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sleep 3 &
-wait; torify ruby testclient.rb &
-wait; kill $(ps a | grep "tor")
