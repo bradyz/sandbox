b36bfba352c7733eea62ac2a4d15a23afb3d7d71
bradyz <brady.zhou@utexas.edu>
2014-10-31 14:01:15 -0500

    BZ added 158

--- codeforce/158/nextround.cpp
+++ codeforce/158/nextround.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <string>
 
 int main () 
 {
   int n, k;
+  int score, temp;
+  int place;
+  int count = 0;
 
+  std::cin >> n >> k;
+
+  for(place = 1; place <= n; place++)
+  {
+    std::cin >> score;
+    if(place == k)
+      temp = score;
+
+    if(score < temp || score == 0)
+    {
+      std::cout << count << std::endl;
+      return 0;
+    } 
+
+    count++;
+  }
+
+  std::cout << count << std::endl;
 
   return 0;
 }
