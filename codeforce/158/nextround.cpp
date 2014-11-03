44fed8d1d8bb1ba8d6236a8bfd7691b344567d29
bradyz <brady.zhou@utexas.edu>
2014-11-02 21:36:08 -0600

    BZ accepted 158A next round

--- codeforce/158/nextround.cpp
+++ codeforce/158/nextround.cpp
@@ -3,21 +3,17 @@
 int main () 
 {
   int n, k;
-  int score, temp;
-  int place;
-  int count = 0; 
+  int score, place, temp = 0;
   std::cin >> n >> k; 
+  int count = n; 
   for(place = 1; place <= n; place++)
   {
     std::cin >> score;
     if(place == k)
-      temp = score; 
-    if(score < temp || score == 0)
-    {
-      std::cout << count << std::endl;
-      return 0;
-    } 
-    count++;
+      temp = score;
+
+    if(score == 0 || ((temp > score) && (temp != 0)))
+      count -= 1;
   } 
   std::cout << count << std::endl; 
   return 0;
