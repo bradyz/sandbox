e6719a3cf0641a6c3be7e20a275174eacc399684
bradyz <brady.zhou@utexas.edu>
2014-11-04 19:38:36 -0600

    BZ did some 158 taxi

--- codeforce/158/taxi.cpp
+++ codeforce/158/taxi.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <map>
 
 int main () 
 {
-  int n, k;
-  int score, place, temp = 0;
-  std::cin >> n >> k; 
-  int count = n; 
-  for(place = 1; place <= n; place++)
-  {
-    std::cin >> score;
-    if(place == k)
-      temp = score;
+  int n;
+  int k;
+  int group_num;
+  int count = 0; 
+  std::map<int, int> group_list;
 
-    if(score == 0 || ((temp > score) && (temp != 0)))
-      count -= 1;
+  //number inputs
+  std::cin >> n; 
+
+  for(k = 0; k < n; k++)
+  {
+    std::cin >> group_num;
+    group_list[group_num] += 1;
   } 
+
+  count += group_list[4];
+
+  if(group_list[1] > 1 && group_list[3] > 1)
+  {
+    
+
+  }
+
+
   std::cout << count << std::endl; 
   return 0;
 }
