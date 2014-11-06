e8ac1dc4bbab3ff61c82848725abd7d9dddaa9e8
bradyz <brady.zhou@utexas.edu>
2014-11-05 21:55:53 -0600

    BZ taxi is a mess test 64

--- codeforce/158/taxi.cpp
+++ codeforce/158/taxi.cpp
@@ -9,23 +9,62 @@ int main ()
   int count = 0; 
   std::map<int, int> group_list;
 
-  //number inputs
-  std::cin >> n; 
+  std::cin >> n;
 
   for(k = 0; k < n; k++)
   {
     std::cin >> group_num;
     group_list[group_num] += 1;
-  } 
-
-  count += group_list[4];
-
-  if(group_list[1] > 1 && group_list[3] > 1)
-  {
-    
-
   }
 
+  count += group_list[4];
+  group_list[4] -= group_list[4];
+
+  if(group_list[3] > 0)
+  {
+    if(group_list[1] > group_list[3])
+    {
+      //if there are more groups of 1 to pair with
+      count += group_list[3];
+      group_list[1] -= group_list[3];
+      group_list[3] -= group_list[3];
+    }
+    else 
+    {
+      //there are less groups of 1
+      group_list[3] -= group_list[1]; 
+      count += group_list[1];
+      group_list[1] -= group_list[1];
+      count += group_list[3];
+    }
+  }
+
+  if(group_list[2] > 0)
+  {
+    count += group_list[2] / 2;
+    if(group_list[2] != 1)
+      group_list[2] -= group_list[2] * 2;
+
+    while(group_list[2] > 0 && group_list[1] > 0)
+    {
+      count += 1;
+      group_list[2] -= group_list[1] * 2;  
+      group_list[1] -= 2;
+    }
+    if(group_list[2] > 0)
+      count += group_list[2];
+  }
+
+  if(group_list[1] > 0)
+  {
+    if(group_list[1] >= 4)
+    {
+      count += group_list[1] / 4;
+      group_list[1] -= group_list[1] * 4;
+    }
+    if(group_list[1] > 0)
+      count += 1;
+  }
 
   std::cout << count << std::endl; 
   return 0;
