21d40707f6807d5b71587b84a86d4f67f3eb9ef9
bradyz <brady.zhou@utexas.edu>
2014-11-26 17:08:34 -0600

    BZ image editor works

--- randoms/ppmeditor/EffectsTest.java
+++ randoms/ppmeditor/EffectsTest.java
@@ -14,7 +14,7 @@ import java.util.Map.Entry;
 public class EffectsTest { public static void main(String args[]) {
 
     ArrayList<File> fileList = new ArrayList<File>();
-    Map<Integer, Map<Integer, Integer>> colorVals = new HashMap<Integer, Map<Integer, Integer>>();
+    Map<Integer, Map<String, Integer>> colorVals = new HashMap<Integer, Map<String, Integer>>();
     int numInputs;
     int len = 0;
     int width = 0;
@@ -36,9 +36,13 @@ public class EffectsTest { public static void main(String args[]) {
     /*   fileList.add(f); */
     /* } */
 
-    File f1 = new File("tinypix.ppm");
-    File f2 = new File("tinypix1.ppm");
-    File f3 = new File("tinypix2.ppm");
+    /* File f1 = new File("tinypix.ppm"); */
+    /* File f2 = new File("tinypix1.ppm"); */
+    /* File f3 = new File("tinypix2.ppm"); */
+
+    File f1 = new File("tetons1.ppm");
+    File f2 = new File("tetons2.ppm");
+    File f3 = new File("tetons3.ppm");
 
     fileList.add(f1);
     fileList.add(f2);
@@ -54,28 +58,28 @@ public class EffectsTest { public static void main(String args[]) {
         fileInput.nextLine();
         String range = fileInput.nextLine();
 
-        int val;
-        Map<Integer, Integer> tmp = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> tmpEntry = new HashMap<Integer, Integer>();
+        int val = 0;
+        String line = "";
 
-        for(int x = 0; x < width * 3 * len; x ++)
+        while(fileInput.hasNextLine())
         {
-          val = fileInput.nextInt();
-          if(!colorVals.containsKey(x)) {
-            tmpEntry = new HashMap<Integer, Integer>();
-            tmpEntry.put(val, 1);
+          Map<String, Integer> tmpEntry = new HashMap<String, Integer>();
+          line = fileInput.nextLine();
+          if(!colorVals.containsKey(val)) {
+            tmpEntry = new HashMap<String, Integer>();
+            tmpEntry.put(line, 1);
           }
           else {
-            tmpEntry = colorVals.get(x);
-            if(!tmp.containsKey(val)) {
-              tmpEntry = new HashMap<Integer, Integer>();
-              tmpEntry.put(val, 1);
+            tmpEntry = colorVals.get(val);
+            if(!tmpEntry.containsKey(line)) {
+              tmpEntry.put(line, 1);
             }
             else {
-              tmpEntry.put(val, tmpEntry.get(val) + 1);
+              tmpEntry.put(line, tmpEntry.get(line) + 1);
             }
           }
-          colorVals.put(x, tmpEntry);
+          colorVals.put(val, tmpEntry);
+          val++;
         }
       } 
       catch (Exception ex) {
@@ -84,38 +88,38 @@ public class EffectsTest { public static void main(String args[]) {
     }
 
     System.out.println("Output File: ");
-    String writeFile = keyboard.nextLine();
+    /* String writeFile = keyboard.nextLine(); */
+    String writeFile = "asdf.ppm";
 
     try {
       FileWriter fw = new FileWriter(writeFile);
+      FileWriter fw2 = new FileWriter("debug.txt");
 
       fw.write("P3\n");
       fw.write(len + " " + width + "\n");
       fw.write("255\n");
 
       int pos = 0;
-      int max = 0;
+      String correct = "";
       int count = 0;
 
-      Map<Integer, Integer> tmp = new HashMap<Integer, Integer>();
+      Map<String, Integer> tmp = new HashMap<String, Integer>();
 
-      for(Entry<Integer, Map<Integer,Integer>> posVal : colorVals.entrySet()) {
+      for(Entry<Integer, Map<String,Integer>> posVal : colorVals.entrySet()) {
         pos = posVal.getKey();
         tmp = posVal.getValue();
 
-        for(Entry<Integer, Integer> valCount : tmp.entrySet()) {
+        for(Entry<String, Integer> valCount : tmp.entrySet()) {
+
+          fw2.write("Line:" + pos + " Val: " + valCount.getValue() + " Count: " + valCount.getKey() + "\n");
           if(valCount.getValue() > count) {
-            max = valCount.getKey();
+            correct = valCount.getKey();
             count = valCount.getValue();
           }
         }
 
-        fw.write(max + " ");
-
+        fw.write(correct + "\n");
         count = 0;
-
-        if((pos + 1) % (3 * width) == 0)
-          fw.write("\n");
       }
 
       fw.close();
