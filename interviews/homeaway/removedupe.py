bc65569658f4edd302f76c07accbc9cee21fe1ea
bradyz <brady.zhou@utexas.edu>
2014-12-28 21:43:38 -0600

    BZ added map method to dupe removal

--- interviews/homeaway/removedupe.py
+++ interviews/homeaway/removedupe.py
@@ -1,8 +1,9 @@
+from collections import Counter
+
+
 def remove_dupe(arr):
     result = []
     arr.sort()
-
-    print arr
     prev = arr[0]
     result.append(arr[0])
 
@@ -16,12 +17,22 @@ def remove_dupe(arr):
     return result
 
 
+def remove_dupe_map(arr):
+    count = Counter()
+
+    for n in arr:
+        count[n] += 1
+
+    return count.keys()
+
+
 if __name__ == "__main__":
     with open('removedupe-input.txt', 'r') as f:
         lines = f.readlines()
 
     for l in lines:
         arr = [int(i) for i in l.split()]
-        print arr
-        print str(remove_dupe(arr))
-        print "\n"
+        print "Arr: " + str(arr)
+        print "Map Method: " + str(remove_dupe_map(arr))
+        print "Sort Method: " + str(remove_dupe(arr))
+        print ""
