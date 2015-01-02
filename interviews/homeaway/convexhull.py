02fee93902e67f38b88c882401b7646d64233687
bradyz <brady.zhou@utexas.edu>
2015-01-01 19:39:34 -0600

    BZ progress on convex hull

--- interviews/homeaway/convexhull.py
+++ interviews/homeaway/convexhull.py
@@ -1,4 +1,4 @@
-import math as math
+import math
 # import matplotlib.pyplot as plt
 
 PI = math.pi
@@ -35,7 +35,6 @@ def rad_to_deg(r):
 
 
 def angle(a, b):
-    # res = math.atan2(b[1] - a[1], b[0] - a[1])
     res = math.atan2(float(b[1]) - float(a[1]), float(b[0]) - float(a[1]))
     if res < 0:
         return res + 2 * PI
@@ -47,18 +46,30 @@ def convex_hull(arr):
     res = []
     a = min(arr, key=lambda x: arr[0])
     res.append(a)
-    b = []
-    while b != a:
-        min_angle = None
+    b = a
+    print a
+    count = 0
+    while count == 0 or b != a:
+        print ""
+        print "b is " + str(b)
+        min_angle = .51 * PI
+        min_coor = [0, 0]
         for y in arr:
-            tmp_angle = angle(y, x)
-            if not min_angle:
-                min_angle = y
-            elif cc_diff(tmp_angle) < cc_diff(min_angle):
-                min_angle = tmp_angle
-                b = y
-                print y
-        res.append(b)
+            if y != b:
+                tmp = angle(b, y)
+                print "b: " + str(b) + " y: " + str(y) + " angle: " + str(tmp)
+
+                if cc_diff(tmp) < cc_diff(min_angle):
+                    # print "New Min"
+                    min_angle = tmp
+                    min_coor = y
+                    count += 1
+                    # print y
+                # print "tmp: " + str(cc_diff(tmp_angle)) + " " + str(tmp_angle)
+                # print "min: " + str(cc_diff(min_angle)) + " " + str(min_angle)
+        count += 1
+        res.append(min_coor)
+        b = min_coor
     return res
 
 
@@ -68,12 +79,15 @@ if __name__ == "__main__":
     for l in lines:
         x = []
         y = []
-        a = l.split()
-        for coor in a:
+        coor_list = l.split()
+        for coor in coor_list:
             xy = coor.split(',')
             x.append(xy[0])
             y.append(xy[1])
         inp = merge_xy(x, y)
+        a = inp[0]
+        # for coor in inp:
+        #     print str(math.degrees(angle(a, coor)))
         print str(convex_hull(inp))
 
     # plt.plot(x, y, 'ro')
