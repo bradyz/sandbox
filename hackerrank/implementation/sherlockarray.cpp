14f9c8461f38a7016482adb6d31e13e9327cec19
bradyz <brady.zhou@utexas.edu>
2015-01-13 22:24:04 -0600

    added cpp example for hr sherlock and array

--- hackerrank/implementation/sherlockarray.cpp
+++ hackerrank/implementation/sherlockarray.cpp
@@ -4,18 +4,6 @@
 
 using namespace std;
 
-#define fore(i, l, r) for(int i = (l); i < (r); ++i)
-#define forn(i, n) fore(i, 0, n)
-#define fori(i, l, r) fore(i, l, (r) + 1)
-
-typedef long long li;
-typedef long double ld;
-typedef pair<int, int> pt;
-
-const int INF = (int)1e9;
-const ld EPS = 1e-9;
-const ld PI = 3.1415926535897932384626433832795;
-
 int readInt(int l, int r){
   int x;
   if(scanf("%d", &x) != 1){
@@ -34,23 +22,34 @@ int readInt(int l, int r){
 
 int main(){
   int T = readInt(1, 10);
-  forn(Ti, T){
+
+  // forn(Ti, T){
+  for(int Ti = 0; Ti < T; Ti++) {
     int n = readInt(1, 100000);
     vector<int> a(n);
-    forn(i, n)
+
+    // forn(i, n)
+    for(int i = 0; i < n; i++)
       a[i] = readInt(1, 20000);
-    vector<li> x(n);
-    forn(i, n){
+
+    vector<long long> x(n);
+
+    // forn(i, n){
+    for(int i = 0; i < n; i++) {
       x[i] = (i == 0 ? 0 : x[i - 1]) + a[i];
     }
-    li sum = 0;
+
+    long long sum = 0;
+
     int cnt = 0;
-    for(int i = n - 1; i >= 0; --i){
-      li left = (i == 0 ? 0 : x[i - 1]);
+
+    for(int i = n - 1; i >= 0; --i) {
+      long long left = (i == 0 ? 0 : x[i - 1]);
       if(left == sum)
         cnt++;
       sum += a[i];
     }
+
     puts(cnt == 0 ? "NO" : "YES");
   }
 
