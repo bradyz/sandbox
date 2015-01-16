e8dd44008530c12b3f0a65a44f8ac6b06b6bc876
bradyz <brady.zhou@utexas.edu>
2015-01-16 10:45:49 -0600

    added binary search

--- hackerrank/arraysort/tutorialintro.py
+++ hackerrank/arraysort/tutorialintro.py
@@ -14,8 +14,21 @@ def n_scan(n, h):
 
 def bin_search(n, h):
     h = sorted(h)
-    mid = len(h) / 2
-    return 0
+    first = 0
+    last = len(h) - 1
+    index = -1
+
+    while first <= last and index == -1:
+        mid = (first + last) / 2
+        if h[mid] == n:
+            index = mid
+        else:
+            if n < h[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+
+    return index
 
 if __name__ == "__main__":
     for i, line in enumerate(sys.stdin):
@@ -27,3 +40,4 @@ if __name__ == "__main__":
             haystack = [int(x) for x in line.split()]
             print(find_needle(needle, haystack))
             print(n_scan(needle, haystack))
+            print(bin_search(needle, haystack))
