6bb237bc765c6dff8b17510055394d76c8731015
bradyz <brady.zhou@utexas.edu>
2015-01-17 22:35:12 -0600

    added hackerrank running time of insertion sort

--- hackerrank/arraysort/insertionsort.py
+++ hackerrank/arraysort/insertionsort.py
@@ -10,9 +10,10 @@ def print_arr(arr):
 
 
 def insertion_sort(a, s):
+    count = 0
     if a == 1:
-        print_arr(a)
-        return
+        # print_arr(a)
+        return count
 
     for x in range(1, s):
         while x > 0 and a[x] < a[x - 1]:
@@ -20,9 +21,10 @@ def insertion_sort(a, s):
             a[x] = a[x - 1]
             a[x - 1] = tmp
             x -= 1
-        print_arr(a)
+            count += 1
+        # print_arr(a)
 
-    return
+    return count
 
 
 def insertion_sort1(a, s):
@@ -44,5 +46,4 @@ if __name__ == "__main__":
             size = int(line.strip("\n"))
         else:
             parsed = [int(x) for x in line.split()]
-            # insertion_sort1(parsed, size)
-            insertion_sort(parsed, size)
+            print(insertion_sort(parsed, size))
