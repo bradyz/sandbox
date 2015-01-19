2ebf95a69469857fb2d53a04907d7fa61fc32d51
bradyz <brady.zhou@utexas.edu>
2015-01-18 21:34:45 -0600

    added hackerrank quicksort 2

--- hackerrank/arraysort/quicksort.py
+++ hackerrank/arraysort/quicksort.py
@@ -5,12 +5,45 @@ def quicksort1(a, s):
     p = a[0]
     less = []
     greater = []
+
     for x in range(s):
         if a[x] < p:
             less.append(a[x])
         elif not a[x] is p:
             greater.append(a[x])
+
     return less + [p] + greater
+
+
+def quicksort(a, s):
+    if s == 1 or s == 0:
+        if s == 0:
+            return [None]
+        else:
+            return [a[0]]
+
+    p = a[0]
+    less = []
+    greater = []
+
+    for x in range(s):
+        if a[x] < p:
+            less.append(a[x])
+        elif not a[x] is p:
+            greater.append(a[x])
+
+    l = quicksort(less, len(less))
+    g = quicksort(greater, len(greater))
+
+    if l == [None]:
+        tmp = [p] + g
+    elif g == [None]:
+        tmp = l + [p]
+    else:
+        tmp = l + [p] + g
+
+    print(" ".join(map(str, tmp)))
+    return tmp
 
 if __name__ == "__main__":
     for i, line in enumerate(sys.stdin):
@@ -18,5 +51,6 @@ if __name__ == "__main__":
             size = int(line.strip("\n"))
         else:
             parsed = [int(x) for x in line.split()]
-            a = quicksort1(parsed, size)
-            print(" ".join(map(str, a)))
+            # a = quicksort1(parsed, size)
+            # print(" ".join(map(str, a)))
+            quicksort(parsed, size)
