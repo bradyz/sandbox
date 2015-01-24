15cc330b59eba3ad02acdb4e3fd76423c7340ea3
bradyz <brady.zhou@utexas.edu>
2015-01-24 13:26:06 -0600

    added hackerrank counting sort 2

--- hackerrank/arraysort/countingsort.py
+++ hackerrank/arraysort/countingsort.py
@@ -16,11 +16,29 @@ def countsort1(arr):
 
     return res
 
+
+def countsort(arr):
+    count = {}
+    res = []
+
+    for x in range(0, 100):
+        count[x] = 0
+
+    for x in arr:
+        count[x] += 1
+
+    for x in range(0, 100):
+        if count[x] != 0:
+            for times in range(count[x]):
+                res.append(x)
+
+    return res
+
 if __name__ == "__main__":
     for i, line in enumerate(sys.stdin):
         if i == 0:
             size = int(line.strip("\n"))
         else:
             parsed = [int(x) for x in line.split()]
-            result = countsort1(parsed)
-            print(result)
+            result = countsort(parsed)
+            print(" ".join(map(str, result)))
