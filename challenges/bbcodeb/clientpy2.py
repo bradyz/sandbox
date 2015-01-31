ea77dd6886c0a3fac2b416d51406b816bf155096
bradyz <brady.zhou@utexas.edu>
2015-01-30 22:30:05 -0600

    added parsed info

--- challenges/bbcodeb/clientpy2.py
+++ challenges/bbcodeb/clientpy2.py
@@ -9,19 +9,17 @@ stocks = ["AAPL", "ATVI", "EA", "FB", "GOOG", "MSFT", "SBUX",
 
 def run(*commands):
     HOST, PORT = "codebb.cloudapp.net", 17429
-
     data = USER + " " + PASS + "\n" + "\n".join(commands)
     data += "\nCLOSE_CONNECTION\n"
-
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         sock.connect((HOST, PORT))
         sock.sendall(data)
         sfile = sock.makefile()
         rline = sfile.readline()
         while rline:
             print(rline.strip())
+            return rline
             rline = sfile.readline()
     finally:
         sock.close()
@@ -29,12 +27,9 @@ def run(*commands):
 
 def subscribe():
     HOST, PORT = "codebb.cloudapp.net", 17429
-
     data = USER + " " + PASS + "\nSUBSCRIBE\n"
-
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         sock.connect((HOST, PORT))
         sock.sendall(data)
         sfile = sock.makefile()
@@ -52,13 +47,39 @@ def trade():
     for tick in stocks:
         i = checkorder(tick)
         print(i)
-    print(stocks)
 
 
 def checkorder(ticker):
     res = "ORDERS " + ticker
-    response = run(res)
+    response = str(run(res)).lstrip("SECURITY_ORDERS_OUT BID ").split()
+    details = []
+
     return response
+
+def ba_profit(ticker):
+    info = checkorder(ticker)
+
+
+def parsed_info():
+    response = str(run("SECURITIES")).lstrip("SECURITIES_OUT ").split()
+    res = {}
+    print(response)
+
+    for x in range(len(response)):
+        if x % 4 == 0:
+            tmp_tick = response[x]
+            res[tmp_tick] = []
+        else:
+            res[tmp_tick].append(response[x])
+
+    print(res)
+    return res
+
+
+def securities():
+    resp = run("SECURITIES")
+    print(resp)
+    return resp
 
 
 def bid(ticker, price, shares):
