6bd84ffb6fdb46c3b6eca2019c0ddec6b45dd03d
bradyz <brady.zhou@utexas.edu>
2015-02-10 15:40:19 -0600

    added permutations function with yield

---
+++ interviews/bloomberg/permutations.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def permutations(a_string):
+    arr = list(a_string)
+    for x in range(len(arr)):
+        for y in _permutations(arr[x], arr[:x] + arr[x+1:]):
+            yield y
+
+
+def _permutations(prefix, arr):
+    if len(arr) == 0:
+        yield prefix
+
+    for x in range(len(arr)):
+        for y in _permutations(prefix + arr[x], arr[:x] + arr[x+1:]):
+            yield y
+
+
+if __name__ == "__main__":
+    for i, line in enumerate(sys.stdin):
+        a_str = line.strip("\n")
+        a = permutations(a_str)
+        for perm in a:
+            print(perm)
