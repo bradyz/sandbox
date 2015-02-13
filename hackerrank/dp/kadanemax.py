aaeb04c2712f10ef18ef6a954177dd6f35b97adc
bradyz <brady.zhou@utexas.edu>
2015-02-12 21:56:12 -0600

    added hackerrank kadane alg

---
+++ hackerrank/dp/kadanemax.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def max_noncont(arr):
+    max_sum = 0
+    for x in arr:
+        if x > 0:
+            max_sum += x
+    if max_sum == 0:
+        max_sum = max(arr)
+    return max_sum
+
+
+def max_sub(arr):
+    max_end = 0
+    max_ = 0
+    for val in arr:
+        max_end = max(0, max_end + val)
+        max_ = max(max_, max_end)
+
+    return max_
+
+if __name__ == "__main__":
+    for i, line in enumerate(sys.stdin):
+        if i > 0:
+            if i % 2 == 0:
+                array = [int(x) for x in line.split()]
+                cont = max_sub(array)
+                noncont = max_noncont(array)
+                print(str(cont) + " " + str(noncont))
