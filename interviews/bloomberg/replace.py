2f5aa65774a96ed4516c3b8b2f23cdce40dae6fb
bradyz <brady.zhou@utexas.edu>
2015-02-14 17:35:52 -0800

    added descriptions to interview questions

--- interviews/bloomberg/replace.py
+++ interviews/bloomberg/replace.py
@@ -1,3 +1,8 @@
+# Replace Description:
+# Given a string, character to replace, and character to sub
+# Return a new string which subbed in characters
+
+
 def sub_letter(a_str, to_r, to_s):
     arr = list(a_str)
     for i in range(len(arr)):
