c9dce2312938e91fb998985bc43da9b282ce8963
bradyz <brady.zhou@utexas.edu>
2015-02-15 20:42:06 -0600

    added int to roman description

--- interviews/twitter/inttoroman.py
+++ interviews/twitter/inttoroman.py
@@ -1,3 +1,7 @@
+# Roman Converter Description:
+# Given a roman numeral,
+# Return the decimal representation
+
 conv = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50,
         "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}
 
