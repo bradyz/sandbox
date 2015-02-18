318c26b5027b9feddf98006ac861e3cf731fc250
bradyz <brady.zhou@utexas.edu>
2015-02-17 23:12:47 -0600

    working on countluck

--- hackerrank/search/countluck.py
+++ hackerrank/search/countluck.py
@@ -1,7 +1,21 @@
 import numpy as np
 
-def num_waves(grid, start, end):
-        return 0
+def num_waves(grid, s, e, rows, cols):
+    valid = []
+    num = 0
+    cur = s
+
+    while cur != e:
+        if cur[0] + 1 < rows and grid[cur[0]+1] == 1:
+            valid.append((cur[0]+1, cur[1]))
+        if cur[0] - 1 >= 0 and grid[cur[0]+1] == 1:
+            valid.append((cur[0]-1, cur[1]))
+        if cur[1] + 1 < cols and grid[cur[0]+1] == 1:
+            valid.append((cur[0], cur[1]+1))
+        if cur[1] - 1 >= 0 and grid[cur[0]+1] == 1:
+            valid.append((cur[0], cur[1]-1))
+
+    return 0
 
 if __name__ == "__main__":
     num_cases = int(input())
@@ -22,5 +36,6 @@ if __name__ == "__main__":
                 elif val == "M":
                     my_grid[r][c] = 1
                     start = (r, c)
-        num_waves = int(input())
+        waves = int(input())
+        actual_waves = num_waves(my_grid, start, end, args[0], arg[1])
         print(my_grid)
