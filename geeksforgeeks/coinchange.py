1300714305be102c72433b1d9e959001e5dddf86
bradyz <brady.zhou@utexas.edu>
2015-02-23 15:30:52 -0600

    added memo to coin change

--- geeksforgeeks/coinchange.py
+++ geeksforgeeks/coinchange.py
@@ -1,16 +1,23 @@
 inf = 1000000
+mem = {}
+
 
 def optimal(i, val):
     if val < 0:
         return inf
-    if i <= 0:
-        return inf
     if val == 0:
         return 0
+    if i < 0:
+        return inf
+
+    if str((i, val)) in mem:
+        return mem[(str((i, val)))]
 
     res = inf
-    res = min(res, optimal(i, val-coins[i]))
+    res = min(res, 1 + optimal(i, val-coins[i]))
     res = min(res, optimal(i-1, val))
+
+    mem[str((i, val))] = res
 
     return res
 
