b9eec4f4379e08e66fabc53fca3d7123497762fb
bradyz <brady.zhou@utexas.edu>
2015-02-25 13:01:20 -0600

    added hackerrank longest increasing nlogn

--- hackerrank/dp/longestincreasing.py
+++ hackerrank/dp/longestincreasing.py
@@ -31,13 +31,39 @@ def long_inc(arr):
             if dp[j] > m and arr[j] < arr[i]:
                 m = dp[j]
         dp[i] = m + 1
-        max_yet = max(max_yet, m)
+        print(dp)
+        max_yet = max(max_yet, dp[i])
     return max_yet
+
+
+def long_nlogn(arr):
+    m = [0 for _ in range(len(arr)+1)]
+    L = 0
+    print(arr)
+
+    for i in range(len(arr)):
+        lo = 1
+        hi = L
+        while lo <= hi:
+            mid = (lo+hi)/2
+            if arr[m[mid]] < arr[i]:
+                lo = mid+1
+            else:
+                hi = mid-1
+        newL = lo
+
+        m[newL] = i
+        L = max(L, newL)
+        # print("I: " + str(i) + " Val: " + str(arr[i]))
+        # print("M: " + str(m))
+
+    return L
+
 
 if __name__ == "__main__":
     my_arr = []
     for i, line in enumerate(sys.stdin):
         if i > 0:
             my_arr.append(int(line))
-    num = long_inc(my_arr)
+    num = long_nlogn(my_arr)
     print(num)
