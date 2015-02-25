553ce294f80b55ef123036b6754e6460590255d4
bradyz <brady.zhou@utexas.edu>
2015-02-25 14:42:53 -0600

    added hackerrank longest common

--- hackerrank/dp/longestcommonsub.py
+++ hackerrank/dp/longestcommonsub.py
@@ -1,19 +1,29 @@
 import sys
+from copy import copy
+import pprint
+# import numpy as np
 
 
 def longest_comm(a, b, m, n):
-    L = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
-    for i in range(m + 1):
-        for j in range(n + 1):
-            print(L)
-            if i == 0 and j == 0:
-                L[i][j] = 0
+    L = [[[] for _ in range(n + 1)] for _ in range(m + 1)]
+    # L = np.zeros([m+1, n+1], int)
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                L[i][j] = []
             elif a[i-1] == b[j-1]:
-                L[i][j] = L[i-1][j-1] + 1
+                tmp = copy(L[i-1][j-1])
+                tmp.append(a[i-1])
+                L[i][j] = tmp
             else:
-                L[i][j] = max(L[i-1][j], L[i][j-1])
+                if len(L[i-1][j]) > len(L[i][j-1]):
+                    tmp = copy(L[i-1][j])
+                    L[i][j] = tmp
+                else:
+                    tmp = copy(L[i][j-1])
+                    L[i][j] = tmp
+            pp.pprint(L)
 
-    print(L)
     return L[m][n]
 
 if __name__ == "__main__":
@@ -24,5 +34,6 @@ if __name__ == "__main__":
             arr_a = [int(x) for x in line.split()]
         else:
             arr_b = [int(x) for x in line.split()]
+            pp = pprint.PrettyPrinter()
             res = longest_comm(arr_a, arr_b, len(arr_a), len(arr_b))
-            print(res)
+            print(" ".join(map(str, res)))
