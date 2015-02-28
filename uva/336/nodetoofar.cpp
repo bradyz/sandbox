da8c15f2324f313e1d0beba8c6e24ca63a381427
bradyz <brady.zhou@utexas.edu>
2015-02-27 18:21:36 -0600

    working on uva 336

---
+++ uva/336/nodetoofar.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <unordered_set> // size, find, insert, erase
+#include <string>
+#include <cstdio> // scanf
+
+using namespace std;
+
+int reach(map <int, vector <int> > graph, int start, int jump) {
+    int tmp;
+    vector <int> tmpv;
+    queue <int> q;
+    map <int, int> v;
+
+    for(map <int, vector <int> >::iterator it = graph.begin(); it != graph.end(); ++it) 
+        v[it->first] = -1;
+
+    q.push(start);
+    v[start] = jump;
+
+    while(!q.empty()) {
+        tmp = q.front();
+        q.pop();
+        tmpv = graph[tmp];
+        for(vector <int>::iterator it = tmpv.begin(); it != tmpv.end(); ++it) {
+            if(v[tmp] - 1 > v[*it]) {
+                cout << *it << " " << v[tmp] - 1 << endl;
+                v[*it] = v[tmp] - 1;
+                q.push(*it);
+            }
+        }
+    }
+
+    int count = 0;
+
+    for(map <int, int>::iterator it = v.begin(); it != v.end(); ++it) 
+    {
+        if(it->second > -1)
+            count += 1;
+    }
+        
+
+    return graph.size() - count;
+}
+
+int main() {
+    int n;
+    int a, b;
+    int s, e;
+    int c = 0;
+    int res = 0;
+
+    while(scanf("%d", &n) && n) {
+        map <int, vector<int> > g;
+        for(int i = 0; i < n; ++i) {
+            scanf("%d %d", &a, &b);
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+
+        while(scanf("%d %d", &s, &e) && (s != 0 && e != 0)) {
+            res = reach(g, s, e);
+            cout << "Case " << ++c << ": " << res << " nodes not reachable from node ";
+            cout << s << " with TTL = " << e << "." << endl;
+        }
+    }
+}
