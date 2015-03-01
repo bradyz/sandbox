8c3dc086da18a43e437a8bc2c985933a98bc6db6
bradyz <brady.zhou@utexas.edu>
2015-02-28 19:10:28 -0600

    added hackerrank priyanka toys accepted

--- hackerrank/greedy/priyankatoys.py
+++ hackerrank/greedy/priyankatoys.py
@@ -7,18 +7,19 @@ def max_toys(n):
     sale = {}
     units = 0
     n = set(n)
+    # print(sorted(n))
 
     for x in n:
         count[x] += 1
 
-    for x in n:
+    for x in count:
         tmp = 0
         for i in range(5):
             tmp += count[x+i]
         sale[x] = tmp
 
     # print(sale)
-    while True:
+    while tmp > 0:
         tmp = 0
         tmpi = 0
         for x in sale:
@@ -27,10 +28,10 @@ def max_toys(n):
                 tmpi = x
         if tmp == 0:
             break
-        for i in range(-1, -5, -1):
-            if tmpi + i in sale:
-                sale[tmpi+i] -= 1
+        # print(tmpi)
         for i in range(5):
+            if tmpi - i in sale:
+                sale[tmpi-i] -= count[tmpi]
             if tmpi + i in sale:
                 sale[tmpi+i] = 0
         units += 1
@@ -39,8 +40,23 @@ def max_toys(n):
     return units
 
 
+def max_t(n):
+    arr = [0 for _ in range(10001)]
+    for x in n:
+        arr[x] += 1
+
+    i = 0
+    count = 0
+    while(i < len(arr)):
+        if arr[i] > 0:
+            count += 1
+            i += 4
+        i += 1
+    return count
+
+
 if __name__ == "__main__":
     for i, line in enumerate(sys.stdin):
         if i == 1:
             prices = [int(x) for x in line.split()]
-            print(max_toys(prices))
+            print(max_t(prices))
