3e818986586a6533e995b156db35f7bf4244b11e
bradyz <brady.zhou@utexas.edu>
2015-03-02 13:42:04 -0600

    added pivot kth largest

--- randoms/kthlargest.py
+++ randoms/kthlargest.py
@@ -1,10 +1,12 @@
+from random import randrange
+
 def kth(a, k):
     def pivot(s, e):
         val = a[s]
         left = s
         right = e
         while left < right:
-            while a[left] <= val:
+            while left < right and a[left] <= val:
                 left += 1
             while a[right] > val:
                 right -= 1
@@ -14,12 +16,28 @@ def kth(a, k):
         a[right] = val
         return right
     l = len(a)
-    pivot(0, l-1)
-    print(a)
+    idx = 0
+    while idx != l - k:
+        tmp = pivot(idx, l-1)
+        print("tmp: " + str(tmp) + " val: " + str(a[tmp]))
+        if tmp > l - k + 1:
+            idx -= 1
+        else:
+            idx += 1
+        print(a)
+
+    return a[-k:]
 
 if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        el = int(input())
-        arr = [int(val) for val in raw_input().split()]
-        print(kth(arr, el))
+    arr = [randrange(100) for _ in range(10)]
+    el = 2
+    print(str(el) + " elements")
+    print(arr)
+    print(kth(arr, el))
+    # t = int(input())
+    # for _ in range(t):
+    #     el = int(input())
+    #     arr = [int(val) for val in raw_input().split()]
+    #     print(el)
+    #     print(arr)
+    #     print(kth(arr, el))
