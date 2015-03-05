7b24a14d6c0b452fe41f8fcf5f306519500cdd9b
bradyz <brady.zhou@utexas.edu>
2015-03-05 16:01:13 -0600

    minimax still runtime error

--- hackerrank/greedy/minimax.py
+++ hackerrank/greedy/minimax.py
@@ -1,26 +1,41 @@
-from collections import Counter
-
-
 def minimax(a, p, q):
     minmax = 0
-    save = 0
     a = list(set(a))
-    count = {}
-    for v in a:
-        count[v] = 1
+    a.sort()
     for x in range(p, q+1):
-        i = 0
-        while x+i not in count and x-1 not in count:
-            i += 1
-        if i > minmax:
-            minmax = i
+        c = bin_search(a, x)
+        tmp = abs(a[c]-x)
+        if c-1 >= 0:
+            tmp = min(tmp, abs(a[c-1]-x))
+        if c+1 < len(a):
+            tmp = min(tmp, abs(a[c+1]-x))
+        if tmp > minmax:
+            minmax = tmp
             save = x
+
     return save
+
+
+def bin_search(a, v):
+    s = 0
+    e = len(a)
+    cur = -1
+    while s <= e:
+        h = (e+s)/2
+        if h == len(a):
+            return h-1
+        cur = a[h]
+        if v == cur:
+            return h
+        elif v < cur:
+            e = h - 1
+        else:
+            s = h + 1
+    return h
 
 if __name__ == "__main__":
     size = int(input())
-    arr = [val for val in raw_input().split()]
-    arr = map(int, arr)
+    arr = [int(val) for val in raw_input().split()]
     pq = [int(val) for val in raw_input().split()]
     start = pq[0]
     end = pq[1]
