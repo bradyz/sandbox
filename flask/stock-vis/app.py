f2cfffde4b36f378d9aae91538774340983aaea3
bradyz <brady.zhou@utexas.edu>
2015-03-07 16:16:40 -0600

    added random gif page

--- flask/stock-vis/app.py
+++ flask/stock-vis/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from stock_scraper import get_data
+
+import requests
+
+giphy_key = "dc6zaTOxFJmzC"
 
 app = Flask(__name__)
 
@@ -12,6 +16,15 @@ def index():
 @app.route("/data")
 def data():
     return jsonify(get_data())
+
+
+@app.route("/gifs", methods=['GET'])
+def gifs():
+    response = requests.get("http://api.giphy.com/v1/gifs/random?api_key="
+                            + giphy_key)
+    gif_url = response.json()['data']['image_url']
+
+    return render_template("gifs.html", gif=gif_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
