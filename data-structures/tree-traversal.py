ae2798b23e0026dad95400bbaac75f7a78d003be
bradyz <brady.zhou@utexas.edu>
2015-03-09 16:21:44 -0500

    added post order

--- data-structures/tree-traversal.py
+++ data-structures/tree-traversal.py
@@ -12,15 +12,14 @@ class node:
         res = []
 
         while len(s) > 0:
-            print(map(str, res))
-            cur = s[-1]
-            if cur.left and cur.left not in res:
-                s.append(cur.left)
-            elif cur.right and cur.right not in res:
-                res.append(s.pop())
+            cur = s.pop()
+            if cur:
                 s.append(cur.right)
+                s.append(cur)
+                s.append(cur.left)
             else:
-                res.append(s.pop())
+                if len(s) > 0:
+                    res.append(s.pop())
 
         return res
 
@@ -29,14 +28,31 @@ class node:
         res = []
 
         while len(s) > 0:
-            print(map(str, res))
             cur = s.pop()
-            res.append(cur)
-            if cur.right:
+            if cur:
+                res.append(cur)
                 s.append(cur.right)
-            if cur.left:
                 s.append(cur.left)
 
+        return res
+
+    def postorder(self):
+        s = [self]
+        res = []
+
+        while len(s) > 0:
+            cur = s.pop()
+            if cur:
+                if cur not in res:
+                    s.append(cur)
+                    s.append(None)
+                if cur.right not in res:
+                    s.append(cur.right)
+                if cur.left not in res:
+                    s.append(cur.left)
+            else:
+                if len(s) > 0 and s[-1]:
+                    res.append(s.pop())
         return res
 
     def level_order(self):
@@ -45,7 +61,6 @@ class node:
         q.put(self)
 
         while not q.empty():
-            print(map(str, res))
             cur = q.get()
             if cur:
                 res.append(cur)
@@ -57,12 +72,29 @@ class node:
     def __str__(self):
         return str(self.val)
 
+    def __int__(self):
+        return self.val
+
 
 if __name__ == "__main__":
     root = node(20, node(8, node(4), node(12, node(10), node(14))), node(22))
-    i = root.inorder()
-    print(map(str, i))
-    l = root.level_order()
-    print(map(str, l))
-    p = root.preorder()
-    print(map(str, p))
+
+    a = root.inorder()
+    print("In Order => [4, 8, 10, 12, 14, 20, 22]")
+    print(map(int, a))
+    print("")
+
+    b = root.level_order()
+    print("Level Order => \t[20, 8, 22, 4, 12, 10, 14]")
+    print(map(int, b))
+    print("")
+
+    c = root.preorder()
+    print("Pre Order => [20, 8, 4, 12, 10, 14, 22]")
+    print(map(int, c))
+    print("")
+
+    d = root.postorder()
+    print("Post Order: [4, 10, 14, 12, 8, 22, 20]")
+    print(map(int, d))
+    print("")
