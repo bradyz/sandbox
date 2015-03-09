2f849e0df948ac5bacbe6facb3d770a5144e4b89
bradyz <brady.zhou@utexas.edu>
2015-03-09 12:37:50 -0500

    added bounds checking

--- interviews/twitter/kth-two.py
+++ interviews/twitter/kth-two.py
@@ -1,23 +1,30 @@
 def kth_large(a, b, k):
-    ai = len(a) - 1
-    bi = len(b) - 1
+    ai = 1
+    bi = 1
 
-    while ai > 0 and bi > 0:
-        if a[ai] > b[bi]:
-            ai -= 1
-            if len(a)-ai+len(b)-bi == k:
-                return a[ai]
+    while ai > 0 or bi > 0:
+        if ai < len(a) and a[-ai] > b[-bi]:
+            if ai + bi - 1 == k:
+                return a[-ai]
+            ai += 1
         else:
-            bi -= 1
-            if len(a)-ai+len(b)-bi == k:
-                return b[bi]
+            if ai + bi - 1 == k:
+                return b[-bi]
+            bi += 1
 
 if __name__ == "__main__":
+    a_arr = [1]
+    b_arr = [0, 2, 4, 6, 8]
+    result = kth_large(a_arr, b_arr, 3)
+    print(a_arr)
+    print(b_arr)
+    print(sorted(a_arr+b_arr))
+    print("3rd Largest: " + str(result))
+
     a_arr = [1, 3, 5, 7, 9]
     b_arr = [0, 2, 4, 6, 8]
     result = kth_large(a_arr, b_arr, 3)
     print(a_arr)
     print(b_arr)
     print(sorted(a_arr+b_arr))
-    print("3rd Largest")
-    print(result)
+    print("3rd Largest: " + str(result))
