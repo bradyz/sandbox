6cfe60ae27aff2060174153b8256d6fe76ba4fd8
bradyz <brady.zhou@utexas.edu>
2015-03-10 01:19:40 -0500

    added lru cache

---
+++ randoms/lru-cache.py
@@ -0,0 +1,88 @@
+class node:
+    def __init__(self, key=None, val=None, head=None, tail=None):
+        self.key = key
+        self.val = val
+        self.head = head
+        self.tail = tail
+
+    def __str__(self):
+        return str(self.val)
+
+
+class LRU:
+    def __init__(self, cap=5):
+        self.cap = cap
+        self.first = None
+        self.last = None
+        self.m = {}
+        print("Creating LRU of Capacity: " + str(self.cap))
+
+    def assign(self, key, val):
+        print("Assigned Key: "+str(key)+" Val: "+str(val))
+        if key in self.m:
+            self.m[key].val = val
+            self.promote(key)
+        else:
+            if len(self.m.keys()) >= self.cap:
+                self.evict()
+            if not self.first:
+                self.first = node(key, val, head=self.last)
+                self.m[key] = self.first
+            elif not self.last:
+                self.last = node(key, val, tail=self.first)
+                self.first.head = self.last
+                self.m[key] = self.last
+            else:
+                self.last.head = node(key, val, tail=self.last)
+                self.last = self.last.head
+                self.m[key] = self.last
+        print(str(self))
+
+    def get(self, key):
+        if key in self.m:
+            self.promote(key)
+            return self.m[key].val
+        return -1
+
+    def evict(self):
+        print("Reached capacity! Evicting...")
+        tmp = self.first.key
+        self.first = self.first.head
+        self.first.tail = None
+        self.m.pop(tmp)
+
+    def promote(self, key):
+        if self.m[key] == self.first:
+            self.first = self.first.head
+        if self.m[key].tail:
+            self.m[key].tail.head = self.m[key].head
+        if self.m[key].head:
+            self.m[key].head.tail = self.m[key].tail
+        self.m[key].head = None
+        self.m[key].tail = self.last
+        self.last.head = self.m[key]
+        self.last = self.m[key]
+
+    def __str__(self):
+        cur = self.first
+        res = []
+        while cur:
+            res.append(cur)
+            cur = cur.head
+        return str(map(str, res))
+
+
+if __name__ == "__main__":
+    a = LRU(4)
+    a.assign(1, 1)
+    a.assign(2, 2)
+    a.assign(3, 3)
+    a.assign(4, 4)
+    a.assign(5, 5)
+    print("Get 2 => " + str(a.get(2)))
+    print(a)
+    for i in range(5, 20):
+        a.assign(i, i)
+        if i % 2:
+            print("Get 2 => " + str(a.get(2)))
+            print(a)
