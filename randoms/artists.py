7391bbb3afceab4cc65df9e6d900804bf199a709
bradyz <brady.zhou@utexas.edu>
2015-03-15 20:22:50 -0500

    added java version of favorite artists

--- randoms/artists.py
+++ randoms/artists.py
@@ -5,21 +5,22 @@ from collections import defaultdict
 def matches(artists):
     fp = {}
     res = set()
+
     for art in artists:
         if len(artists[art]) > 50:
             fp[art] = artists[art]
 
-    for i in fp:
-        for j in fp:
-            if i != j:
-                if len(fp[i] & fp[j]) >= 50 and str(sorted([i, j])) not in res:
-                    res.add(str(sorted([i, j])))
-    print(res)
+    for i in sorted(fp):
+        for j in sorted(fp):
+            if i < j and len(fp[i] & fp[j]) >= 50:
+                    res.add(str((i, j)))
+
+    for a in sorted(res):
+        print(a)
 
 if __name__ == "__main__":
     occ = defaultdict(set)
     for i, line in enumerate(sys.stdin):
-        tmp = line.split(",")
-        for art in tmp:
+        for art in line.split(","):
             occ[art].add(i)
     matches(occ)
