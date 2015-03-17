bb32d3231cd06823e4e96da39a99e5e19f91111d
bradyz <bradyz>
2015-03-17 01:14:43 -0500

    added hackerrank grid challenge

---
+++ hackerrank/greedy/grid-challenge.py
@@ -0,0 +1,18 @@
+def is_possible(grid, row):
+    grid = [[y for y in x] for x in grid]
+    for r in range(row):
+        grid[r].sort()
+    for c in range(row):
+        for r in range(1, row):
+            if grid[r][c] < grid[r-1][c]:
+                return "NO"
+    return "YES"
+
+if __name__ == "__main__":
+    tests = int(input())
+    for _ in range(tests):
+        g = []
+        rows = int(input())
+        for _ in range(rows):
+            g.append(input())
+        print(is_possible(g, rows))
