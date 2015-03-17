294f2ae41f85f775ba7f24748000d694e9ba70ec
bradyz <brady.zhou@utexas.edu>
2015-03-17 01:21:26 -0500

    added hackerrank orders with correct email

--- hackerrank/greedy/orders.py
+++ hackerrank/greedy/orders.py
@@ -1,6 +1,6 @@
 def serve(o):
     o = sorted(o, key=lambda x: (x[1]+x[2], x[2]))
-    print(" ".join(map(str, [x[0] for x in o])))
+    return " ".join(map(str, [x[0] for x in o]))
 
 if __name__ == "__main__":
     n = int(input())
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     for i in range(n):
         se = [int(x) for x in input().split()]
         orders.append((i+1, se[0], se[1]))
-    serve(orders)
+    print(serve(orders))
