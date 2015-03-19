d92e0f95d9fa28c41cf32093f0790712af01c9ca
bradyz <brady.zhou@utexas.edu>
2015-03-18 22:18:47 -0500

    added prime factorization

---
+++ geeksforgeeks/prime-factorization.py
@@ -0,0 +1,14 @@
+def prime_factorization(num):
+    i = 2
+    res = [1]
+    while num != 1:
+        if num % i == 0:
+            num = num / i
+            res.append(i)
+        else:
+            i += 1
+    return res
+
+if __name__ == "__main__":
+    print(prime_factorization(8))
+    print(prime_factorization(100110102))
