e2b8edc3e4dc4d2a4285327b7855c1dd9cabf1e4
bradyz <brady.zhou@utexas.edu>
2015-03-24 00:58:53 -0500

    added o(n^4 log n) solution t.t

--- hackerrank/strings/sherlock-anagrams.py
+++ hackerrank/strings/sherlock-anagrams.py
@@ -1,7 +1,13 @@
 def sub_anagrams(a):
-    print(a)
+    count = 0
+    for i in range(len(a)):
+        for s in range(1, len(a)):
+            for j in range(i+1, len(a)-s+1):
+                if sorted(a[i:i+s]) == sorted(a[j:j+s]):
+                    count += 1
+    print(count)
 
 
 if __name__ == "__main__":
-    n = [input() for _ in range(int(input()))]
-    "\n".join(list(map(sub_anagrams, n)))
+    for _ in range(int(input())):
+        sub_anagrams(list(input()))
