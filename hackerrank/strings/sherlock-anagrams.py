9bcb2b429eb35d4832596bd8601e48f1648615c6
bradyz <brady.zhou@utexas.edu>
2015-03-24 01:10:02 -0500

    added hackerrank sherlock anagrams

--- hackerrank/strings/sherlock-anagrams.py
+++ hackerrank/strings/sherlock-anagrams.py
@@ -1,9 +1,13 @@
+from collections import Counter
+
+
 def sub_anagrams(a):
     count = 0
     for i in range(len(a)):
         for s in range(1, len(a)):
+            c_i = Counter(a[i:i+s])
             for j in range(i+1, len(a)-s+1):
-                if sorted(a[i:i+s]) == sorted(a[j:j+s]):
+                if c_i == Counter(a[j:j+s]):
                     count += 1
     print(count)
 
