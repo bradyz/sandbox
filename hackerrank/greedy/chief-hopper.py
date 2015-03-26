874501b2e57b2bffc8871cc8801942b87005e027
bradyz <brady.zhou@utexas.edu>
2015-03-25 19:59:09 -0500

    added hackerrank chief hopper

--- hackerrank/greedy/chief-hopper.py
+++ hackerrank/greedy/chief-hopper.py
@@ -3,13 +3,18 @@ from math import ceil
 
 def chief_hopper(arr):
     m = 0
-    mi = 0
+
     for i in range(len(arr)-1):
-        if ceil((arr[i]+arr[i+1])/2) > m:
-            mi = i
-            m = max(ceil((arr[i]+arr[i+1])/2), m)
-    for i in range(mi, 0, -1):
+        tmp = ceil((arr[i] + arr[i+1]) / 2)
+        if tmp > m:
+            m = tmp
+
+    if m > 0:
+        m -= arr[len(arr)-1]
+
+    for i in range(len(arr), 0, -1):
         m = ceil((m + arr[i-1]) / 2)
+
     print(m)
 
 if __name__ == "__main__":
