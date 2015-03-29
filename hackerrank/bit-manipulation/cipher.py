b2d903074d58089bb3d2723704d5755e75efe821
bradyz <brady.zhou@utexas.edu>
2015-03-28 20:51:25 -0500

    added cipher implementation with timeouts

--- hackerrank/bit-manipulation/cipher.py
+++ hackerrank/bit-manipulation/cipher.py
@@ -1,10 +1,14 @@
 def cipher(c, l, k):
-    res = int(c+"0"*k, 2)
-    for i in range(1, k+1):
-        res ^= int("0" * k + c + "0" * (k-i), 2)
-    return 0
+    res = [c[0]]
+    for i in range(1, l):
+        tmp = c[i]
+        for j in range(min(k-1, len(res))):
+            tmp ^= res[-(j+1)]
+        res.append(tmp)
+
+    print("".join(list(map(lambda x: str(int(x)), res))))
 
 if __name__ == "__main__":
     arg = [int(x) for x in input().split()]
-    _c = input()
-    cipher(_c, arg[0], arg[1])
+    _c = [int(x) for x in input()]
+    cipher(list(map(bool, _c)), arg[0], arg[1])
