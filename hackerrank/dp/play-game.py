60f25f813219b6280b864c8f57c2dad1762311ef
bradyz <brady.zhou@utexas.edu>
2015-03-29 01:06:14 -0500

    added hackerrank sansa xor

---
+++ hackerrank/dp/play-game.py
@@ -0,0 +1,9 @@
+def play_game(n, c):
+    return 0
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        _n = int(input())
+        _c = [int(x) for x in input().split()]
+        play_game(_n, _c)
