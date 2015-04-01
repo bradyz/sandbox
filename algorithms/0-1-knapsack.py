6154c6349a7754c99f85edc417ab7d5480aa225b
bradyz <brady.zhou@utexas.edu>
2015-04-01 00:13:22 -0500

    added 0-1 knapsack

--- algorithms/0-1-knapsack.py
+++ algorithms/0-1-knapsack.py
@@ -1,11 +1,14 @@
 def knapsack(w, b, c):
-    dp = [[0 for _ in range(len(w)+1)] for _ in range(len(b)+1)]
-    for i in range(len(w) + 1):
-        for j in range(len(b) + 1):
+    dp = [[0 for _ in range(c+1)] for _ in range(len(b)+1)]
+    for i in range(len(b) + 1):
+        for j in range(len(w) + 1):
             if i > 0 and j > 0:
-                x = dp[i-1][j] + w[i-1]*b[i-1]
-                y = dp[i][j-1] + w[i-1]*b[i-1]
-                dp[i][j] = max(dp[i-1][j-1], x, y)
+                x = dp[i-1][j] + b[i-1]
+                y = dp[i][j-1] + b[i-1]
+                if j + w[i-1] < c:
+                    dp[i][j] = max(x, y, dp[i-1][j-1])
+                else:
+                    dp[i][j] = max(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
 
     print(dp)
 
