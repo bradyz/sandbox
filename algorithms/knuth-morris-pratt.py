21a82ebb9cf3833ccd46086614a03309fc749366
bradyz <brady.zhou@utexas.edu>
2015-04-02 21:47:36 -0500

    added comments

--- algorithms/knuth-morris-pratt.py
+++ algorithms/knuth-morris-pratt.py
@@ -1,10 +1,15 @@
 # Knuth-Morris-Pratt String Searching Algorithm
 # n*r construction of DFA (deterministic finite-state automaton)
 # O(n+m) string search
+# Advantages:       Trivial after DFA is constructed
+#                   Worst case O(n+m)
+# Disadvantages:    DFA size is dependent on radix-size and pattern size
+#                   Could be very large for large alphabets(unicode)
 
 
 def kmp(pat, txt):
     def construct():
+        # essentially the alphabet, all possible choices
         con = sorted(set(txt))
         d = {}
 
@@ -14,7 +19,7 @@ def kmp(pat, txt):
             for i in range(len(pat)):
                 d[c][i] = 0
 
-        # heavy lifting, x is restart state
+        # x is restart state
         x = 0
         d[pat[0]][0] = 1
 
@@ -23,7 +28,10 @@ def kmp(pat, txt):
             for j in range(len(con)):
                 d[con[j]][i] = d[con[j]][x]
 
+            # a correct match moves onto the next state
             d[pat[i]][i] = i+1
+
+            # update fail state
             x = d[pat[i]][x]
 
         return d
@@ -32,9 +40,10 @@ def kmp(pat, txt):
     j = 0
 
     for i in range(len(txt)):
+        # jump from state to state
         j = dfa[txt[i]][j]
 
-        # reaches end state of dfa
+        # reaches end state of dfa - a match
         if j == len(a):
             return i - len(pat) + 1
 
