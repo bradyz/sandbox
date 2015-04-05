574cfe0abfc65a4ec9cae74b480dd61827ca5fc5
bradyz <brady.zhou@utexas.edu>
2015-04-04 20:12:11 -0500

    added first implementation of 1d moving range sum

--- algorithms/moving-average.py
+++ algorithms/moving-average.py
@@ -11,17 +11,25 @@ def brute_force(c, k):
 
 def moving_av(c, k):
     avs = [0 for _ in range(len(c)+2*k)]
+    for i in range(len(c)):
+        avs[i+k] = c[i]
 
     avs[k] = sum(c[:k+1])
 
     print(avs)
 
-    for i in range(1, len(c)):
-        avs[i+k] = avs[i+k-1] + c[i] - avs[i-k]
+    for i in range(k-1, len(c)):
+        print(avs[i+k-1], avs[i+k+k])
+        avs[i+k] = avs[i+k-1] + avs[i+k+k]
+
+        if i - k - 1 >= 0:
+            avs[i+k] -= avs[i] - avs[i-1]
+            print(avs[i], avs[i-1])
+
         print(avs)
 
 
 if __name__ == "__main__":
-    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    b = moving_av(a, 1)
-    c = brute_force(a, 1)
+    a = [1, 1, 1, 1, 1, 1, 1, 1]
+    b = moving_av(a, 2)
+    c = brute_force(a, 2)
