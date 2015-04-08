b95e801e33d148b850890532b42a0369a93aa565
bradyz <brady.zhou@utexas.edu>
2015-04-07 23:44:48 -0500

    added summed area table

--- challenges/ut-acm5/blur.py
+++ challenges/ut-acm5/blur.py
@@ -1,3 +1,7 @@
+from copy import deepcopy
+from pprint import PrettyPrinter
+
+
 def brute_force(g, n, m, k):
     new_grid = []
     b = 2*k+1
@@ -19,6 +23,41 @@ def brute_force(g, n, m, k):
 
     return
 
+def summed_area(g, n, m, k):
+    g1 = [[0 for _ in range(m)] for __ in range(n)]
+    g2 = deepcopy(g1)
+
+    for i in range(n):
+        for j in range(m):
+            g1[i][j] = g[i][j]
+            if i > 0:
+                g1[i][j] += g1[i-1][j]
+            if j > 0:
+                g1[i][j] += g1[i][j-1]
+            if i > 0 and j > 0:
+                g1[i][j] -= g1[i-1][j-1]
+
+    # for row in g1:
+    #     print(" ".join(map(str, row)))
+
+    for i in range(n):
+        for j in range(m):
+            a = g1[max(0, i-k-1)][max(0, j-k-1)]
+            b = g1[max(0, i-k-1)][min(m-1, j+k)]
+            c = g1[min(n-1, i+k)][max(0, j-k-1)]
+            d = g1[min(n-1, i+k)][min(m-1, j+k)]
+            if i-k-1 < 0:
+                a = 0
+                b = 0
+            if j-k-1 < 0:
+                a = 0
+                c = 0
+            g2[i][j] = round((a+d-c-b) / (2*k+1)**2)
+
+    for row in g2:
+        print(" ".join(map(str, row)))
+
+
 for _ in range(int(input())):
     args = [int(v) for v in input().split()]
     nA = args[0]
@@ -30,3 +69,4 @@ for _ in range(int(input())):
         grid.append([int(v) for v in input().split()])
 
     brute_force(grid, nA, mA, kA)
+    summed_area(grid, nA, mA, kA)
