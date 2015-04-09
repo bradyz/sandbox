8c1429e0a88e1effa896dec4a5f0c87220039032
bradyz <brady.zhou@utexas.edu>
2015-04-08 19:58:18 -0500

    modify moving average

--- algorithms/moving-average.py
+++ algorithms/moving-average.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+
 # O(n^2) adding up averages
 def brute_force(c, k):
     res = [v for v in c]
@@ -14,21 +17,26 @@ def brute_force(c, k):
 
 # O(k+n) adding up averages
 def moving_av(c, k):
-    avs = [0 for val in c]
-    avs[0] = sum(c[:k+1])
-
     for i in range(1, len(c)):
-        avs[i] = avs[i-1]
-        if i+k < len(c):
-            avs[i] += c[i+k]
-        if i-(k+1) >= 0:
-            avs[i] -= c[i-k-1]
+        c[i] += c[i-1]
 
-    print(avs)
+    d = copy(c)
 
+    for i in range(len(d)):
+        if i-k-1 < 0:
+            start = 0
+        else:
+            start = c[i-k-1]
+        if i+k >= len(d):
+            end = d[-1]
+        else:
+            end = c[i+k]
+        d[i] = end - start
+
+    print(d)
 
 if __name__ == "__main__":
     a = [2, 4, 6, 8, 10, 12, 14, 16]
     print(a)
-    b = moving_av(a, 2)
-    c = brute_force(a, 2)
+    y = brute_force(a, 2)
+    x = moving_av(a, 2)
