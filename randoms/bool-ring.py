c33332f32b758e5e1225c1c3a0127d7a40878835
bradyz <brady.zhou@utexas.edu>
2015-04-09 02:20:27 -0500

    added bool ring axiom 2

--- randoms/bool-ring.py
+++ randoms/bool-ring.py
@@ -2,9 +2,9 @@
 # ===================
 # plus
 # 0   1   x   y
-# 1   0
-# x       0
-# y           0
+# 1   0   y   x
+# x   y   0
+# y   x       0
 # plus1
 # 0   1   x   y
 # 1   0   y   x
@@ -15,6 +15,11 @@
 # 1   0   y   x
 # x   y   0   1
 # y   x   1   0
+# plus3
+# 0   1   x   y
+# 1   0   x   y
+# x   x   0   1
+# y   y   1   0
 
 # Multiplication
 # ===================
@@ -30,8 +35,8 @@ plus = {}
 times = {}
 
 for i in range(len(r)):
-    plus[frozenset([r[i], "0"])] = "0"
-    plus[frozenset(r[i])] = r[i]
+    plus[frozenset([r[i], "0"])] = r[i]
+    plus[frozenset(r[i])] = "0"
 
 for i in range(len(r)):
     times[frozenset([r[i], "0"])] = "0"
@@ -44,10 +49,12 @@ plus[frozenset(["1", "y"])] = "x"
 
 plus1 = deepcopy(plus)
 plus2 = deepcopy(plus)
+plus3 = deepcopy(plus)
 
 plus[frozenset(["x", "y"])] = "x"
 plus1[frozenset(["x", "y"])] = "y"
 plus2[frozenset(["x", "y"])] = "1"
+plus3[frozenset(["x", "y"])] = "0"
 
 
 def a2(_r, _p):
@@ -59,11 +66,13 @@ def a2(_r, _p):
                 c = _r[k]
                 ab = _p[frozenset([a, b])]
                 bc = _p[frozenset([b, c])]
-                if _p[frozenset([ab, c])] != _p[frozenset([a, bc])]:
-                    print(a, b, c)
+                ab_c = _p[frozenset([ab, c])]
+                a_bc = _p[frozenset([a, bc])]
+                if ab_c != a_bc:
                     return False
     return True
 
 print("plus: " + str(a2(r, plus)))
 print("plus1: " + str(a2(r, plus1)))
 print("plus2: " + str(a2(r, plus2)))
+print("plus3: " + str(a2(r, plus3)))
