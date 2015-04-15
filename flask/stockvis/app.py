11d88cdefb5cac2b35c3e4dabe9642d6aac7ff42
bradyz <brady.zhou@utexas.edu>
2015-04-15 00:57:58 -0500

    added browser-sync

--- flask/stockvis/app.py
+++ flask/stockvis/app.py
@@ -72,4 +72,4 @@ def get_info(ticker):
 #     return ""
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
--- flask/stockvis/gulpfile.js
+++ flask/stockvis/gulpfile.js
@@ -1,17 +1,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var process = require('child_process');
+var lr = require('tiny-lr'),
+    server = lr();
 
-gulp.task('sass', function() { 
-  return gulp.src('static/css/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('static/css')); 
+var browserSync = require('browser-sync'),
+    reload = browserSync.create().reload;
+
+
+gulp.task('styles', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(sass({ style: 'expanded', 'sourcemap=none': true }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest('static/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('static/css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:5001",
+    files: ["**/*.*"],
+    browser: "google chrome",
+    port: 7000,
+  });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('static/css/**/*.scss', ['sass']); 
+  livereload.listen({start: true});
+  gulp.watch('sass/*.scss', ['styles']); 
+  gulp.watch('templates/*.html').on('change', reload);
 });
 
-gulp.task('default', ['sass', 'watch'], function(){
-  var process = require('child_process');
+gulp.task('default', ['styles', 'browser-sync', 'watch'], function(){
   process.spawn('python3', ['app.py'], {stdio: 'inherit'});
 });
