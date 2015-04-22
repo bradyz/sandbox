1342e11b3d9da3faf86f7801ec245a052c7395fb
bradyz <brady.zhou@utexas.edu>
2015-04-22 01:26:07 -0500

    add polynomial of degree 2

--- machine-learning/polynomial-regression.py
+++ machine-learning/polynomial-regression.py
@@ -1,19 +1,29 @@
 import numpy as np
 import pylab as pl
 
+# make sure points are not really random
 np.random.seed(42)
+
+# array of 20 random points
 x = np.random.random(20)
 y = np.sin(2 * x)
-p = np.polyfit(x, y, 1)
 
-x_p = np.random.random(3)
-y_p = np.polyval(p, x_p)
+# linear regression of y with respect to x
+p_1 = np.polyfit(x, y, 1)
+p_2 = np.polyfit(x, y, 2)
 
+# values
 xfit = np.linspace(0, 1, 1000)
-yfit = np.polyval(p, xfit)
+yfit = np.polyval(p_1, xfit)
+y1fit = np.polyval(p_2, xfit)
+
+# axis and config
 pl.scatter(x, y, c='k')
 pl.xlabel('x')
 pl.ylabel('y')
+
+pl.plot(xfit, np.sin(2 * xfit))
 pl.plot(xfit, yfit)
+pl.plot(xfit, y1fit)
 
 pl.show()
