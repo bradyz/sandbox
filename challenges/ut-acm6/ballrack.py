96275f513151655714595aea13ac6103857cb511
bradyz <brady.zhou@utexas.edu>
2015-04-24 21:13:47 -0500

    added test cases for acm ball rack

--- challenges/ut-acm6/ballrack.py
+++ challenges/ut-acm6/ballrack.py
@@ -3,17 +3,20 @@ from math import ceil
 
 for i in range(int(input())):
     n = int(input())
-    r = [float(v) for v in input().split()]
-    res = r[0]
-    for j in range(1, n):
+    r = [int(v) for v in input().split()]
+    x = [a for a in r] + [r[-1]]
+
+    print(x)
+    for j in range(n):
         if j == 0:
-            res += r[j]
+            if n > 1 and sqrt((r[j] + r[j+1])**2 - (r[j] - r[j+1])**2) < r[j+1]:
+                x[j] = 0
         else:
             tmp = sqrt((r[j] + r[j-1])**2 - (r[j] - r[j-1])**2)
-            if tmp < r[j] or tmp < r[j-1]:
-                res += max(r[j], r[j-1])
-            else:
-                res += sqrt((r[j] + r[j-1])**2 - (r[j] - r[j-1])**2)
-            if j == n - 1:
-                res += r[j]
-    print(ceil(res))
+            x[j] = max(tmp, r[j], r[j-1])
+            if j == n-1 and tmp > r[j] and r[j-1] > r[j]:
+                x[j+1] = 0
+        print(x)
+
+    print(ceil(sum(x)))
+    print()
