965396ca2823b07d8d42b7bbc020d311c30c776b
bradyz <brady.zhou@utexas.edu>
2015-04-27 17:56:28 -0500

    working on gift wrap

--- interviews/homeaway/convexhull.py
+++ interviews/homeaway/convexhull.py
@@ -3,82 +3,49 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-PI = math.pi
 
-
-def cc_diff(a, b=.5*PI):
-    if a >= 0 and a <= b:
-        return b - a
-    else:
-        return 2 * PI + b - a
-
-
-def angle_pos(x):
-    if x < 0:
-        return 360 + x
-    else:
-        return x
-
-
-def merge_xy(x, y):
-    coor = []
-    if len(x) != len(y):
-        return []
-    for i in range(len(x)):
-        tmp = []
-        tmp.append(x[i])
-        tmp.append(y[i])
-        coor.append(tmp)
-    return coor
-
-
-def rad_to_deg(r):
-    return r * 180 / math.pi
-
-
-def angle(a, b):
-    res = math.atan2(float(b[1]) - float(a[1]), float(b[0]) - float(a[0]))
-    if res < 0:
-        return res + 2 * PI
-    else:
-        return res
+def angle(x, y):
+    res = math.atan2(float(y[1]) - float(x[1]), float(y[0]) - float(x[0]))
+    return res % (2 * math.pi)
 
 
 def convex_hull(arr):
     a = min(arr, key=lambda x: x[0])
-    res = []
+    res = [a]
+
     b = a
-    print('Start: ' + str(a))
-    count = 0
-    while not res or b != a:
-        min_angle = .51 * PI
-        min_coor = None
-        max_angle = .49 * PI
-        max_coor = None
-        for y in arr:
-            if y != b:
-                tmp = angle(b, y)
+    c_d = 0
+    c_a = 2 * math.pi
 
-                if not min_coor:
-                    min_coor = b
-                if not max_coor:
-                    max_coor = b
+    while len(res) == 1 or b is not a:
+        for c in arr:
+            if c is not b:
+                d = angle(b, c)
+                if d - c_d > 0 and d - c_d < c_a:
+                    c_a = c_d - d
+                    c_d = d
+                    n_b = c
+            b = n_b
+            c_d = c_a
+        if b is n_b:
+            print(b, n_b)
+            break
+        b = n_b
+        res.append(b)
+        print("tmp: " + str(res))
 
-                cond = y not in res or y == a
-                if cc_diff(tmp) < cc_diff(min_angle) and cond:
-                    min_angle = tmp
-                    min_coor = y
-                if cc_diff(tmp) > cc_diff(max_angle) and cond:
-                    max_angle = tmp
-                    max_coor = y
-        count -= 1
-        res.append((b, min_coor))
-        b = min_coor
+    print('Res: ' + str(res))
+
+    plt.plot(a[0], a[1], "ro")
+    plt.plot([x[0] for x in res], [x[1] for x in res], "b--")
+
     return res
 
 
 if __name__ == "__main__":
     t = np.random.rand(10, 2)
+    t = [[1, 1], [0, 2], [1, 3], [2, 1], [3, 2], [2, 3]]
+    n = len(t)
 
     hull = ConvexHull(t, 2)
 
@@ -88,8 +55,12 @@ if __name__ == "__main__":
     x_rim += [x_rim[0]]
     y_rim += [y_rim[0]]
 
-    plt.plot(t[:, 0], t[:, 1], 'bo')
-    plt.plot(x_rim, y_rim, "r--")
-    plt.plot(t[hull.vertices[0], 0], t[hull.vertices[0], 1], 'ro')
+    plt.plot([t[i][0] for i in range(n)], [t[i][1] for i in range(n)], 'bo')
+    # plt.plot(x_rim, y_rim, "r--")
+
+    plt.ylim([0, 4])
+    plt.xlim([-1, 4])
+
+    convex_hull(t)
 
     plt.show()
