8893875e50656ea221d466f4e890e90fff55777c
bradyz <brady.zhou@utexas.edu>
2015-04-27 19:39:06 -0500

    added jarvis march gift wrapping

--- interviews/homeaway/convexhull.py
+++ interviews/homeaway/convexhull.py
@@ -6,9 +6,7 @@ import numpy as np
 
 def angle(x, y):
     res = math.atan2(float(y[1]) - float(x[1]), float(y[0]) - float(x[0]))
-    if res >= math.pi:
-        res -= 2 * math.pi
-    return res
+    return res % (2 * math.pi)
 
 
 def convex_hull(arr):
@@ -18,26 +16,23 @@ def convex_hull(arr):
     b = a
     c_ang = 1.5 * math.pi
 
-    while len(res) == 1 or b is not a:
+    while len(res) == 1 or not np.array_equal(b, a):
         m_diff = 2 * math.pi
         for c in arr:
-            if c is not b:
+            if not np.array_equal(c, b):
                 tmp_a = angle(b, c)
 
-                if c_ang - tmp_a > 0 and c_ang - tmp_a < m_diff:
-                    m_diff = c_ang - tmp_a
+                if (c_ang - tmp_a) % (2 * math.pi) < m_diff:
+                    m_diff = (c_ang - tmp_a) % (2 * math.pi)
                     new_b = c
                     tmp_c = tmp_a
-
-        if b is new_b:
-            break
 
         b = new_b
         c_ang = tmp_c
 
         res.append(b)
 
-    res += [res[0]]
+    # res += [res[0]]
 
     plt.plot(a[0], a[1], "ro")
     plt.plot([x[0] for x in res], [x[1] for x in res], "b--")
@@ -46,8 +41,8 @@ def convex_hull(arr):
 
 
 if __name__ == "__main__":
-    t = np.random.rand(10, 2) * 5
-    # t = [[1, 1], [0, 2], [1, 3], [2, 1], [3, 2], [2, 3], [1, 2], [2, 2]]
+    t = [[1, 1], [0, 2], [1, 3], [2, 1], [3, 2], [2, 3], [1, 2], [2, 2]]
+    t = np.random.rand(100, 2) * 50
     n = len(t)
 
     hull = ConvexHull(t, 2)
@@ -61,8 +56,8 @@ if __name__ == "__main__":
     plt.plot([t[i][0] for i in range(n)], [t[i][1] for i in range(n)], 'bo')
     # plt.plot(x_rim, y_rim, "r--")
 
-    plt.ylim([-1, 6])
-    plt.xlim([-1, 6])
+    plt.ylim([-1, 60])
+    plt.xlim([-1, 60])
 
     convex_hull(t)
 
