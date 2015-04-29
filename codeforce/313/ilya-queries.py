099b01c8909c4f1bb87bcb1a2503b598bfbe64bc
bradyz <brady.zhou@utexas.edu>
2015-04-29 13:59:01 -0500

    tle on codeforces 313b (dp)

---
+++ codeforce/313/ilya-queries.py
@@ -0,0 +1,15 @@
+# 313B: Ilya Queries
+# Start Time: 12:46 a.m. 4-28-15
+# End Time: 10:50 a.m. 4-28-15
+
+query = input()
+
+dp = [0 for _ in range(len(query) + 1)]
+
+for i in range(1, len(query)+1):
+    if i > 1:
+        dp[i] = dp[i-1] + int(query[i-1] == query[i-2])
+
+for _ in range(int(input())):
+    q = list(map(int, input().split()))
+    print(dp[q[1]] - dp[q[0]])
