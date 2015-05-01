e89b294110ccb8ac2deb0974ad77a2a80954f3c6
bradyz <brady.zhou@utexas.edu>
2015-05-01 14:10:33 -0500

    codeforces 75c accepted python (nt)

--- codeforce/75/modified-gcd.py
+++ codeforce/75/modified-gcd.py
@@ -1,9 +1,9 @@
 # 75C: Modified GCD
 # Start Time: 12:39 p.m. 5-1-15
-# End Time: 10:50 a.m. 4-28-15
+# End Time: 2:09 p.m. 5-1-15
 
-from math import sqrt, ceil
 from collections import Counter
+from bisect import bisect_right as br
 
 saved = {}
 
@@ -26,6 +26,20 @@ def pf(num):
     return ps
 
 
+# all the factors of a number in sqrt(n) time
+def f(num):
+    i = 1
+    res = []
+    while i * i <= num:
+        if num % i == 0:
+            res.append(num // i)
+            if i != num // i:
+                res.append(i)
+        i += 1
+    return sorted(res)
+
+
+# O(n^1.5) tle at 1:30 p.m. 5-1-15
 def solve(c, x, y):
     for i in range(y, x-1, -1):
         if i in saved and saved[i] != -1:
@@ -35,13 +49,19 @@ def solve(c, x, y):
             return i
         else:
             saved[i] = -1
+
     return -1
+
+
+def solve1(c, x, y):
+    res = c[br(c, y)-1]
+    return res if res >= x else -1
 
 if __name__ == "__main__":
     args = map(int, input().split())
     n1, n2 = args
-    g = pf(gcd(n1, n2))
+    g = f(gcd(n1, n2))
 
     for _ in range(int(input())):
         r1, r2 = map(int, input().split())
-        print(solve(g, r1, r2))
+        print(solve1(g, r1, r2))
