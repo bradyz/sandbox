ac4c5e1bbd8e7c6bc3d36bc80f473c2cb3066977
bradyz <brady.zhou@utexas.edu>
2015-05-02 01:55:41 -0500

    added minkowski sum

--- geometry/test-point.py
+++ geometry/test-point.py
@@ -13,13 +13,16 @@ class TestPoint(unittest.TestCase):
 
 class TestLine(unittest.TestCase):
 
+    # ****************************************
+    # *             point_given              *
+    # ****************************************
+
     def test_point_given_x(self):
         a = Line(Point(0, 0), Point(5, 5))
         self.assertEqual(Point(2, 2), a.point_given_x(2))
 
     def test_point_given_x_vertical(self):
         a = Line(Point(0, 0), Point(0, 5))
-        print(a.point_given_x(2))
         self.assertEqual(None, a.point_given_x(2))
 
     def test_point_given_y(self):
@@ -30,14 +33,57 @@ class TestLine(unittest.TestCase):
         a = Line(Point(0, 0), Point(0, 5))
         self.assertEqual(None, a.point_given_y(2))
 
+    # ****************************************
+    # *                 on                   *
+    # ****************************************
+
+    def test_on_true(self):
+        a = Point(2, 2)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertTrue(a.on(b))
+
+    def test_on_false(self):
+        a = Point(0, 1)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertFalse(a.on(b))
+
+    # ****************************************
+    # *             left_of                  *
+    # ****************************************
+
     def test_left_of(self):
         a = Point(0, 1)
         b = Line(Point(0, 0), Point(1, 1))
         self.assertTrue(a.left_of(b))
 
-    @unittest.skip("not implemented")
-    def test_left_of_horizontal(self):
-        self.assertTrue(True)
+    def test_left_of_on(self):
+        a = Point(2, 2)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertTrue(a.left_of(b))
+
+    def test_left_of_false(self):
+        a = Point(2, 1)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertFalse(a.left_of(b))
+
+    # ****************************************
+    # *             right_of                 *
+    # ****************************************
+
+    def test_right_of(self):
+        a = Point(2, 1)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertTrue(a.right_of(b))
+
+    def test_right_of_on(self):
+        a = Point(2, 2)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertTrue(a.right_of(b))
+
+    def test_right_of_false(self):
+        a = Point(0, 1)
+        b = Line(Point(0, 0), Point(1, 1))
+        self.assertFalse(a.right_of(b))
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
