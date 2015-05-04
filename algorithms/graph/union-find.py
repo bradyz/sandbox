d6e3b51b211284f73b1f3e2f38bebba66ab6ac13
bradyz <brady.zhou@utexas.edu>
2015-05-04 15:11:40 -0500

    added comments and rename

--- algorithms/graph/union-find.py
+++ algorithms/graph/union-find.py
@@ -11,12 +11,12 @@ class edge:
         return str(self._a) + " " + str(self._b)
 
 
+# O(n) complexity, depends on find
 def union(p, x, y):
-    x_p = find(p, x)
-    y_p = find(p, y)
-    p[x_p] = y_p
+    p[find(p, x)] = find(p, y)
 
 
+# O(n) complexity, has to go through at most all nodes
 def find(p, i):
     if(p[i] == -1):
         return i
@@ -28,22 +28,30 @@ def has_cycle(g, p):
         x = find(p, e._a)
         y = find(p, e._b)
 
+        # if the two points belong to the same subset
         if x == y:
             return True
         else:
             union(p, x, y)
+            # p[x] = y equivalently, since x and y are parents
 
     return False
 
 if __name__ == "__main__":
     n = 3
 
+    # graph vis
+    # 0
+    # | \
+    # 1 - 2
     edge_1 = edge(0, 1)
     edge_2 = edge(1, 2)
     edge_3 = edge(0, 2)
 
-    graph = [edge_1, edge_3, edge_2]
+    # adjacency graph
+    graph = [edge_1, edge_2, edge_3]
 
+    # initialize all parents to -1
     parents = [-1 for _ in range(n)]
 
     print(has_cycle(graph, parents))
