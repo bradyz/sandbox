28076d9ee2530bb2ddb4db9574d1b3c3f9aeb12e
bradyz <brady.zhou@utexas.edu>
2015-05-05 14:34:24 -0500

    added factor utility file

---
+++ algorithms/number-theory/factor.py
@@ -0,0 +1,27 @@
+def prime_factors(n):
+    p = set()
+    i = 2
+    while n > 1:
+        if n % i == 0:
+            p.add(i)
+            n //= i
+        else:
+            i += 1
+    return sorted(p)
+
+
+def factors(n):
+    f = set()
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            f.add(n // i)
+            f.add(i)
+        i += 1
+    return sorted(f)
+
+
+if __name__ == "__main__":
+    print(prime_factors(10))
+    print(prime_factors(128))
+    print(factors(128))
