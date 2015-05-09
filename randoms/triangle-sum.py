cdb3dfb3c6731e61bcebfdffbad34fcbe1683053
bradyz <brady.zhou@utexas.edu>
2015-05-08 21:00:42 -0500

    dp solution to triangle sum

--- randoms/triangle-sum.py
+++ randoms/triangle-sum.py
@@ -1,3 +1,5 @@
+import sys
+
 # Find the maximum weighted path to a leaf
 #         1
 #        / \
@@ -27,16 +29,40 @@ def child(idx, total):
 
 def max_val(v, n):
     def dfs(c_i, cur):
-        c[c_i] = max(c[c_i], cur + v[c_i])
-        for i in child(c_i, n):
-            if i != -1:
-                dfs(i, c[c_i])
+        if c[c_i] < cur + v[c_i]:
+            c[c_i] = cur + v[c_i]
+            for i in child(c_i, n):
+                if i != -1:
+                    dfs(i, c[c_i])
 
     c = [-1 for _ in range(n)]         # cumulative sums
     dfs(0, 0)
     print(c)
 
 
+def solve(v, n):
+    dp = [[-1 for j in range(n)] for i in range(n)]
+
+    for i in range(n):
+        for j in range(len(v[i])):
+            if i == 0 and j == 0:
+                dp[i][j] = v[i][j]
+            elif j == 0:
+                dp[i][j] = dp[i-1][j] + v[i][j]
+            elif j == len(v[i]):
+                dp[i][j] = dp[i-1][j-1] + v[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + v[i][j]
+
+    print(max(dp[n-1]))
+
+
 if __name__ == "__main__":
-    values = [1, 6, 4, 8, 9, 3, 9, 12, 6, 7]
-    max_val(values, len(values))
+    values = []
+    i = 0
+
+    for line in sys.stdin:
+        i += 1
+        values.append(list(map(int, line.split())))
+
+    solve(values, i)
