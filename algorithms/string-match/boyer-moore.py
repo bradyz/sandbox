efa082f16a2ed621197c2c8ff363ace3b570caf3
bradyz <brady.zhou@utexas.edu>
2015-05-09 19:54:33 -0500

    boyer moore string matching

---
+++ algorithms/string-match/boyer-moore.py
@@ -0,0 +1,30 @@
+def boyermoore(n, h):
+    right = {}
+
+    for i in range(len(n)):
+        right[n[i]] = i
+
+    i = 0
+    found = False
+
+    print(h)
+
+    while i < len(h) and not found:
+        print(" " * i + h[i:i+len(n)])
+        found = True
+        for j in range(len(n)-1, -1, -1):
+            if n[j] != h[i+j]:
+                if h[i+j] in right:
+                    i += max(1, j - right[h[i+j]])
+                else:
+                    i += len(n)
+                found = False
+                break
+
+    if found:
+        print(i, h[i:i+len(n)])
+
+if __name__ == "__main__":
+    needle = "needle"
+    haystack = "findingahaystackneedleina"
+    boyermoore(needle, haystack)
