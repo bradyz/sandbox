eff37f32a8ec56bd6ed962c8253a69cd5e7b39e9
bradyz <brady.zhou@utexas.edu>
2015-05-19 15:07:04 -0500

    on click alert i javascript

---
+++ randoms/javascript/events.js
@@ -0,0 +1,11 @@
+var attach = function(nodes) {
+  for(var i = 0; i < nodes.length; i += 1) {
+    nodes[i].onclick = function(i) {
+      return function(e) {
+        alert(i);
+      };
+    }(i);
+  }
+};
+
+attach(document.body.childNodes);
