c062f7779cee8c7df3f553457fdb10126c207745
bradyz <brady.zhou@utexas.edu>
2015-05-19 14:01:50 -0500

    scopes and closures js
    learning squash
    
    squash 2

---
+++ randoms/javascript/scope.js
@@ -0,0 +1,18 @@
+var myObject1 = function() {
+  var val = 0;                      // scoped within function
+
+  return {
+    increment: function(inc) {
+      return val += typeof inc === 'number' ? inc : 1;
+    },
+    getVal: function() {
+      return val;
+    }
+  }
+}();                                 // note the two parenthesis
+
+// myObject.getVal() = 2;             // doesn't compile
+// console.log(myObject1.val);        // undefined, hidden by scope
+console.log(myObject1);               // { increment: [Function], getVal: [Function] }
+console.log(myObject1.getVal());      // 0 
+console.log(myObject1.increment());   // 1 
