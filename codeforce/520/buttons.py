085062f66e095484cee4ae8cfd9d46628d63d16d
bradyz <brady.zhou@utexas.edu>
2015-05-20 16:00:23 -0500

    codeforces 520b using bfs

--- codeforce/520/buttons.py
+++ codeforce/520/buttons.py
@@ -1,11 +1,28 @@
-n, m = map(int, input().split())
-c = 0
+def solve1(n, m):
+    c = 0
+    while n < m:
+        if m % 2 == 0:
+            m //= 2
+        else:
+            m += 1
+        c += 1
+    print(n - m + c)
 
-while n < m:
-    if m % 2 == 0:
-        m //= 2
-    else:
-        m += 1
-    c += 1
 
-print(n - m + c)
+def solve2(n, m):
+    from queue import Queue
+    q = Queue()
+    q.put((n, 0))
+
+    while not q.empty():
+        c, d = q.get()
+        if c == m:
+            print(d)
+            break
+        q.put((c-1, d+1))
+        q.put((c*2, d+1))
+
+
+a, b = map(int, input().split())
+# solve1(a, b)
+solve2(a, b)
