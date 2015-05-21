0ce15c944291ac6e6cb51ef43e0bb27331953e9b
bradyz <brady.zhou@utexas.edu>
2015-05-21 04:59:28 -0500

    codeforces 115a party python accepted

--- codeforce/115/party.py
+++ codeforce/115/party.py
@@ -1,10 +1,21 @@
 if __name__ == "__main__":
     t = int(input())
     c = [int(input()) for _ in range(t)]
-    v = [1 for _ in range(t)]
+    v = [[] for _ in range(t+1)]
+
     for i in range(t):
         if c[i] == -1:
-            v[i] = v[c[i]-1] + 1
+            v[0].append(i+1)
         else:
-            v[i] = 1
-    print(max(v))
+            v[c[i]].append(i+1)
+
+    s = [(val, 1) for val in v[0]]
+    m = 0
+
+    while s:
+        cur = s.pop()
+        m = max(cur[1], m)
+        for n in v[cur[0]]:
+            s.append((n, cur[1]+1))
+
+    print(m)
