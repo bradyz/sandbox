c534721e540e7d13d5e2384959bdc3f305e23762
bradyz <brady.zhou@utexas.edu>
2015-05-22 19:25:52 -0500

    annotations to max flow

--- algorithms/graph/fordfulkerson.py
+++ algorithms/graph/fordfulkerson.py
@@ -4,37 +4,41 @@ from sys import maxsize
 
 
 def ford_fulkerson(a_graph, s, t):
+    # typical bfs
     def bfs():
         q = Queue()
-        v = set()
+        vis = set()
         q.put(s)
-        v.add(s)
+        vis.add(s)
         parent[s] = -1
 
         while not q.empty():
             cur = q.get()
 
             for i in range(len(r_graph)):
-                if i not in v and r_graph[cur][i] > 0:
+                if i not in vis and r_graph[cur][i] > 0:
                     q.put(i)
-                    v.add(i)
-                    parent[i] = cur
+                    vis.add(i)
+                    parent[i] = cur                 # create the path
 
-        return t in v
+        return t in v                               # can reach the sink
 
-    r_graph = deepcopy(graph)
-    parent = [0 for _ in range(len(r_graph))]
+    r_graph = deepcopy(graph)                       # residual capacity graph
+    parent = [0 for _ in range(len(r_graph))]       # trace of path for bfs
     max_flow = 0
 
+    # while there exists an augmenting path
     while(bfs()):
         path_flow = maxsize
         v = t
+        # look for the bottleneck capacity
         while v != s:
             u = parent[v]
             path_flow = min(path_flow, r_graph[u][v])
             v = parent[v]
 
         v = t
+        # update the residual graph
         while v != s:
             u = parent[v]
             r_graph[u][v] -= path_flow
@@ -43,6 +47,7 @@ def ford_fulkerson(a_graph, s, t):
 
         max_flow += path_flow
 
+    # use to see the residual graph, the actual flows through edges
     from pprint import PrettyPrinter
     pp = PrettyPrinter()
 
@@ -50,6 +55,7 @@ def ford_fulkerson(a_graph, s, t):
     print(max_flow)
 
 if __name__ == "__main__":
+    # adjacency graph
     graph = [[0, 16, 13, 0, 0, 0],
              [0, 0, 10, 12, 0, 0],
              [0, 4, 0, 0, 14, 0],
@@ -57,4 +63,5 @@ if __name__ == "__main__":
              [0, 0, 0, 7, 0, 4],
              [0, 0, 0, 0, 0, 0]]
 
+    # source of 0, sink of 5
     ford_fulkerson(graph, 0, 5)
