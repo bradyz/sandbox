b3870a89370065da3ec332d45777997def939248
bradyz <brady.zhou@utexas.edu>
2015-05-24 20:33:00 -0500

    javascript memoized fibonacci

---
+++ randoms/javascript/memoizer.js
@@ -0,0 +1,18 @@
+var memoizer = function(memo, fundamental) {
+  var shell = function(n) {
+    var result = memo[n];
+
+    if(typeof result !== 'number') { 
+      result = fundamental(shell, n);
+      memo[n] = result;
+    }
+    return result;
+  };
+  return shell;
+};
+
+var fibonacci = memoizer([0, 1], function (shell, n) {
+   return shell(n - 1) + shell(n - 2);
+});
+
+console.log(fibonacci(10));
