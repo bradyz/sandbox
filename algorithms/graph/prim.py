51cb9766450b9e5883acda08e3a8018dc060efb4
bradyz <brady.zhou@utexas.edu>
2015-05-26 23:38:13 -0500

    prim mst (for real)

--- algorithms/graph/prim.py
+++ algorithms/graph/prim.py
@@ -12,8 +12,14 @@ def min_key(k, m):
             m_i = i
             m_v = k[i]
 
-    print("min: " + str(m_i))
     return m_i
+
+
+# x => node
+# y => parent of node
+# z => edge
+def belongs_to(x, y, z):
+    return (z.a.v == x and z.b.v == y) or (z.a.v == y and z.b.v == x)
 
 
 def prim(g, p, n):
@@ -21,6 +27,7 @@ def prim(g, p, n):
     mst = [False for _ in range(n)]
     key = [MAXINT for _ in range(n)]
     parent = [None for _ in range(n)]
+    r = []
 
     key[0] = 0                  # make sure the first vertex is picked
     parent[0] = -1              # first node is root of MST
@@ -29,14 +36,20 @@ def prim(g, p, n):
     for e in g:
         m[e.a.v][e.b.v] = m[e.b.v][e.a.v] = e
 
+    # look for the minimum edge to add
     for i in range(n):
-        u = min_key(key, mst)
-        mst[u] = True
+        u = min_key(key, mst)   # find minimum node to add
+        mst[u] = True           # add point to mst set
+
+        # expand the cut
         for v in range(n):
-            if not mst[v] and m[u][v] and m[u][v].w < key[u]:
+            if not mst[v] and m[u][v] and m[u][v].w < key[v]:
                 parent[v] = u
                 key[v] = m[u][v].w
-                print(parent, key)
 
-    for i in range(n):
-        print(str(i) + " " + str(parent[i]))
+    for i in range(1, n):
+        for e in g:
+            if belongs_to(i, parent[i], e):
+                r.append(e)
+
+    return r
