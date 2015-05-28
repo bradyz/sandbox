0253d7f85acbfe5a3ae750ccdf9c041491b9bfa1
bradyz <brady.zhou@utexas.edu>
2015-05-27 23:21:41 -0500

    annotations for dijkstra

--- algorithms/graph/dijkstra.py
+++ algorithms/graph/dijkstra.py
@@ -3,11 +3,13 @@ from sys import maxsize as INT_MAX
 
 # O(V^2) time complexity
 def dijkstra(graph, src, n):
+
     # returns the minimum closest vertex not yet visited
     def min_dist():
         min_v = INT_MAX
         min_i = -1
 
+        # looks for minimum index in updated distance array
         for i in range(n):
             if not spt[i] and dist[i] < min_v:
                 min_v = dist[i]
@@ -15,15 +17,16 @@ def dijkstra(graph, src, n):
 
         return min_i
 
-    dist = [INT_MAX for _ in range(n)]
-    spt = [False for _ in range(n)]
+    spt = [False for _ in range(n)]     # included in solution
+    dist = [INT_MAX for _ in range(n)]  # distances from source
+    dist[src] = 0                       # starts from the source
 
-    dist[src] = 0
-
+    # O(V) for each vertex
     for i in range(n):
-        u = min_dist()
-        spt[u] = True
+        u = min_dist()                  # will start from source
+        spt[u] = True                   # mark solution found
 
+        # O(V) that can be easily improved with heap data structure
         for v in range(n):
             if not spt[v] and graph[u][v] and dist[u]+graph[u][v] < dist[v]:
                 dist[v] = dist[u] + graph[u][v]
@@ -33,14 +36,14 @@ def dijkstra(graph, src, n):
         print(i, dist[i])
 
 if __name__ == "__main__":
-    graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-             [4, 0, 8, 0, 0, 0, 0, 11, 0],
-             [0, 8, 0, 7, 0, 4, 0, 0, 2],
-             [0, 0, 7, 0, 9, 14, 0, 0, 0],
-             [0, 0, 0, 9, 0, 10, 0, 0, 0],
-             [0, 0, 4, 0, 10, 0, 2, 0, 0],
-             [0, 0, 0, 14, 0, 2, 0, 1, 6],
-             [8, 11, 0, 0, 0, 0, 1, 0, 7],
-             [0, 0, 2, 0, 0, 0, 6, 7, 0]]
+    adj_graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+                 [4, 0, 8, 0, 0, 0, 0, 11, 0],
+                 [0, 8, 0, 7, 0, 4, 0, 0, 2],
+                 [0, 0, 7, 0, 9, 14, 0, 0, 0],
+                 [0, 0, 0, 9, 0, 10, 0, 0, 0],
+                 [0, 0, 4, 0, 10, 0, 2, 0, 0],
+                 [0, 0, 0, 14, 0, 2, 0, 1, 6],
+                 [8, 11, 0, 0, 0, 0, 1, 0, 7],
+                 [0, 0, 2, 0, 0, 0, 6, 7, 0]]
 
-    dijkstra(graph, 0, len(graph))
+    dijkstra(adj_graph, 0, len(adj_graph))
