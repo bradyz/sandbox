a13126c662167f2715e7d95bd9babc5253272a47
bradyz <brady.zhou@utexas.edu>
2015-05-29 00:53:54 -0500

    codeforces 500 more compact python

--- codeforce/500/permutation1.py
+++ codeforce/500/permutation1.py
@@ -1,24 +1,21 @@
-from sys import maxsize as MAX_INT
-
-
-def dfs(c):
-    global x, vis
-    if v[c] not in u:
-        x = min(x, v[c])
-    vis.add(c)
-    for j in range(n):
-        if g[c][j] == "1" and j not in vis:
-            dfs(j)
-
 n = int(input())
 v = list(map(int, input().split()))
 g = [list(input()) for _ in range(n)]
-u = set()
+u = [False for _ in range(n+1)]
 r = [0 for _ in range(n)]
+w = [-1 for _ in range(n+1)]
+s = []
 for i in range(n):
-    x = MAX_INT
-    vis = set()
-    dfs(i)
+    x = n
+    s.append(i)
+    while s:
+        c = s.pop()
+        if not u[v[c]]:
+            x = min(x, v[c])
+        w[c] = i
+        for j in range(n):
+            if g[c][j] == "1" and w[j] != i:
+                s.append(j)
     r[i] = x
-    u.add(x)
+    u[x] = True
 print(" ".join(list(map(str, r))))
