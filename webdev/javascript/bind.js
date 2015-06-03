80fc52ad34b67a4d7858ce263d2dfbf601924224
bradyz <brady.zhou@utexas.edu>
2015-06-02 19:24:59 -0500

    javascript bindAll example

--- webdev/javascript/bind.js
+++ webdev/javascript/bind.js
@@ -1,5 +1,7 @@
+// has bind and bindall
 var _ = require('underscore');
 
+// basic constructor
 function Developer (name) {
   this.name = name;
   this.says = function () {
@@ -7,9 +9,20 @@ function Developer (name) {
   };
 }
 
+// create instance
 var brady = new Developer('brady');
-var speak = brady.says;
-var another = _.bind(speak, {name: 'greg'});
 
-brady.speak();                                  // brady
-another();                                      // greg
+var speak = brady.says;
+var speak1 = _.bind(speak, {name: 'greg'});
+var speak2 = brady.says;
+
+// binds brady object to all future says references
+_.bindAll(brady, 'says');                       
+
+var speak3 = brady.says;
+
+brady.says();                                   // brady
+speak();                                        // undefined
+speak1();                                       // greg
+speak2();                                       // undefined
+speak3();                                       // brady
