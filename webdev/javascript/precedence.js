7a23db09921b56e5b8c2aa9689d17b7d7fb9ba34
bradyz <brady.zhou@utexas.edu>
2015-06-07 11:33:45 -0400

    javascript regex and precedence

--- webdev/javascript/precedence.js
+++ webdev/javascript/precedence.js
@@ -6,12 +6,24 @@ var Ninja = function () {
   this.checkTruth = function () { 
     return this.truth;
   };
+
+  this.dontThrow = function () {
+    console.log('All is good');
+  };
 };
 
 Ninja.prototype.checkTruth = function () { 
   return !this.truth;
 };
 
+Ninja.prototype.dontThrow = function () { 
+  throw {
+    name: 'BasicError',
+    message: 'Does it reach here?'
+  };
+};
+
 var lulu = new Ninja();
 
 console.log(lulu.checkTruth());                   // true
+lulu.dontThrow();                                 // all is good
