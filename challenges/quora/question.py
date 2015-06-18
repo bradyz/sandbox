a7de57a5fab59cac888baee5b6d71c0278c297c8
bradyz <brady.zhou@utexas.edu>
2015-06-18 00:39:35 -0400

    floyd warshall all pairs shortest path

--- challenges/quora/question.py
+++ challenges/quora/question.py
@@ -1,17 +1,26 @@
 import sys
+from pprint import PrettyPrinter
+pp = PrettyPrinter()
+
 for i, line in enumerate(sys.stdin):
     if i == 0:
         n = int(line)
+        g = [[False for j in range(n+1)] for i in range(n+1)]
     elif i == 1:
-        c = [sys.maxsize] + list(map(int, line.split()))
-        d = [0] + c[1:]
+        c = [0] + list(map(int, line.split()))
+        d = [[999 for j in range(n+1)] for i in range(n+1)]
+        for i in range(1, n+1):
+            d[i][i] = c[i]
     else:
         t = list(map(int, line.split()))
-        d[t[0]] += c[t[1]]
-        d[t[1]] += c[t[0]]
-        print(d)
-m = 0
-for i in range(1, n):
-    if d[i] < d[m]:
-        m = i
-print(m)
+        g[t[0]][t[1]] = g[t[0]][t[1]] = True
+
+l = []
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if g[i][j]:
+                d[i][j] = d[j][i] = min(d[i][j], d[i][i] + c[j])
+            d[i][j] = d[j][i] = min(d[i][j], d[i][k] + d[k][j] - c[k])
+            pp.pprint(d)
