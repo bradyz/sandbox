a89c953676c9a0d784363e4114f14898d42796e8
bradyz <brady.zhou@utexas.edu>
2015-06-25 23:00:32 -0400

    codecon another compliance accepted

--- challenges/codecon/complianceanother.py
+++ challenges/codecon/complianceanother.py
@@ -1,19 +1,11 @@
 from collections import Counter
-from itertools import permutations
-o = 0
-e = 0
-i = input()
-r = Counter(i)
-for v in r.values():
-    if v % 2 == 1:
-        o += 1
-    else:
-        e += 1
-if o <= 1:
-    x = "".join([r[v] // 2 * v for v in r if r[v] % 2 == 0])
+from math import factorial
+r = Counter(input())
+if sum(v % 2 == 1 for v in r.values()) <= 1:
+    t = [v // 2 for v in r.values()]
+    x = factorial(sum(t))
+    for v in t:
+        x //= factorial(v)
     print(x)
-    for v in sorted(list(set(permutations(x)))):
-        print(v)
-    print(len(set(permutations(x))))
 else:
     print(0)
