a9196e120874aa19fbded58bd8515fd6d5bb23cb
bradyz <brady.zhou@utexas.edu>
2015-07-16 02:24:48 -0400

    codecon puzzle triangle try 3 accepted

--- challenges/codecon/puzzlechallenge/trianglev2.py
+++ challenges/codecon/puzzlechallenge/trianglev2.py
@@ -2,21 +2,23 @@ import sys
 
 g = [line.strip() for line in sys.stdin]
 n = len(g)
+m = len(g[0])
 r = 0
 
+
 def solve():
-    global g, n, r
+    global g, n, m, r
     for i in range(n):
-        for j in range(n):
+        for j in range(m):
             t = g[i][j]
-            for k in range(1, n-j+1):
+            for k in range(1, m-j+1):
                 x = 0
-                while x <= k and j+x < n and i-x >= 0 and i-k+x < n and j+x < n and g[i][j+x] == t and g[i-x][j] == t and g[i-k+x][j+x] == t:
+                while x <= k and i-x >= 0 and i-k+x >= 0 and i-k+x < n and j+x < m and g[i][j+x] == t and g[i-x][j] == t and g[i-k+x][j+x] == t:
                     x += 1
                 if x > k:
                     r = max(r, x * (x+1) // 2)
                 x = 0
-                while x <= k and j+x < n and i-x >= 0 and j+k < n and i-x >= 0 and j+x < n and g[i][j+x] == t and g[i-x][j+k] == t and g[i-x][j+x] == t:
+                while x <= k and i-x >= 0 and j+x < m and j+k < m and g[i][j+x] == t and g[i-x][j+k] == t and g[i-x][j+x] == t:
                     x += 1
                 if x > k:
                     r = max(r, x * (x+1) // 2)
