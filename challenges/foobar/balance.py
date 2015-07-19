77d7b1435bf1f01fab79d9619902e5f40b901746
bradyz <brady.zhou@utexas.edu>
2015-07-19 17:37:46 -0400

    foobar balance one WA

--- challenges/foobar/balance.py
+++ challenges/foobar/balance.py
@@ -1,26 +1,43 @@
-def solve(n):
+def solve(c):
     t = 0
-    while 3**(t+1) < n:
+    while 3**t < c:
         t += 1
-    print(t)
     r = ["-" for _ in range(t+1)]
-    while n != 0 and t >= 0:
-        if abs(n) < 3 ** t:
-            pass
-        elif abs(n) > 3 ** t and abs(n) < 2 * 3 ** (t+1):
-            r[t] = "L"
-            if n > 0:
-                n -= 3 ** (t+1)
-            else:
-                n += 3 ** (t+1)
-        else:
-            r[t] = "R"
-            if n > 0:
-                n -= 3 ** t
-            else:
-                n += 3 ** t
-        t -= 1
-        print(n, t, r)
-    return r
 
+    def s(n):
+        if n == 0:
+            return
+
+        t = 0
+        while 3**(t+1) <= abs(n):
+            t += 1
+
+        if 3 ** t <= abs(n) and 2 * 3 ** t > abs(n):
+            if n < 0:
+                r[t] = "L"
+                s(n + 3 ** t)
+            else:
+                r[t] = "R"
+                s(n - 3 ** t)
+        else:
+            if n < 0:
+                r[t+1] = "L"
+                s(n + 3 ** (t+1))
+            else:
+                r[t+1] = "R"
+                s(n - 3 ** (t+1))
+
+    s(c)
+
+    i = len(r)-1
+
+    while i >= 0 and r[i] == "-":
+        i -= 1
+
+    return r[:i+1]
+
+print(solve(8))
 print(solve(10))
+print(solve(3))
+print(solve(2))
+print(solve(12))
