def288a43b256e94540dfc05ce6c471a8a189290
bradyz <brady.zhou@utexas.edu>
2015-07-24 00:07:54 -0400

    foobar lesson passed

--- challenges/foobar/lessonv2.py
+++ challenges/foobar/lessonv2.py
@@ -14,24 +14,42 @@ def answer(c):
         else:
             return 1
 
+    def comparatorV2(x, y):
+        if y in w[x]:
+            return -1
+        else:
+            return 1
+
     v = {}
+    w = {}
 
-    for i in range(len(c)):
-        for j in range(len(c[i])):
-            if c[i][j] not in v:
-                v[c[i][j]] = set()
+    def seed():
+        def dfs(x):
+            w[k] |= v[x]
+            for y in v[x]:
+                dfs(y)
 
-    for i in range(len(c)):
-        for j in range(i+1, len(c)):
-            k = 0
-            while k < min(len(c[i]), len(c[j])) and c[i][k] == c[j][k]:
-                k += 1
-            if k < min(len(c[i]), len(c[j])):
-                v[c[i][k]].add(c[j][k])
+        for i in range(len(c)):
+            for j in range(len(c[i])):
+                if c[i][j] not in v:
+                    v[c[i][j]] = set()
+                if c[i][j] not in w:
+                    w[c[i][j]] = set()
 
-    print(c)
-    print(v)
-    return "".join(sorted(v.keys(), cmp=comparator))
+        for i in range(len(c)):
+            for j in range(i+1, len(c)):
+                k = 0
+                while k < min(len(c[i]), len(c[j])) and c[i][k] == c[j][k]:
+                    k += 1
+                if k < min(len(c[i]), len(c[j])):
+                    v[c[i][k]].add(c[j][k])
+
+        for k in v:
+            dfs(k)
+
+    seed()
+
+    return "".join(sorted(v.keys(), cmp=comparatorV2))
 
 
 print(answer(raw_input().split()))
