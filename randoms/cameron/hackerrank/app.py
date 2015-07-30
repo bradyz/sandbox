abb6e5427730d21a9f4e77cb13085996599382fe
bradyz <brady.zhou@utexas.edu>
2015-07-29 20:23:54 -0400

    hackerrank api connected

--- randoms/cameron/hackerrank/app.py
+++ randoms/cameron/hackerrank/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 
 import requests
-from json import dumps
+import json
 
 
 app = Flask(__name__)
@@ -10,8 +10,8 @@ app.config.from_object("config")
 
 
 @app.route("/")
-def api_root():
-    return render_template("index.html")
+def api_root(message=None):
+    return render_template("index.html", msg=message)
 
 
 @app.route("/submit/", methods=["POST"])
@@ -19,21 +19,21 @@ def api_submit_question():
     code = request.form["code"]
     test_cases = [request.form["test case"]]
     lang = 5
-    headers = {'Content-Type': 'application/json'}
 
     payload = {"source": code,
                "api_key": app.config["HR_KEY"],
                "lang": lang,
-               "testcases": test_cases}
-    print(payload)
-    print(app.config["HR_URL"])
+               "testcases": json.dumps(test_cases)}
 
-    response = requests.post(app.config["HR_URL"], data=payload, headers=headers)
-    print(response.status_code)
-    response = requests.post(app.config["HR_URL"], json=dumps(payload), headers=headers)
-    print(response.status_code)
+    try:
+        response = requests.post(app.config["HR_URL"], data=payload)
+        res_json = json.loads(response.text)
+        msg = res_json["result"]["message"][0]
+    except Exception as e:
+        msg = "Exception - check console"
+        print(e)
 
-    return redirect(url_for('api_root'))
+    return api_root(msg)
 
 if __name__ == "__main__":
     app.run(debug=True)
