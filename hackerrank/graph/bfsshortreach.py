2623b9d575bb24cbcfeabe605a181715ddca9581
bradyz <brady.zhou@utexas.edu>
2015-08-09 22:31:15 -0400

    hackerrank graph bfs V2

--- hackerrank/graph/bfsshortreach.py
+++ hackerrank/graph/bfsshortreach.py
@@ -13,32 +13,32 @@ def bfs():
             continue
         for v in c:
             if v[0] == x[0] and v[1] not in x[2]:
-                if r[x[1]] == -1 or x[1]+w < r[x[1]]:
+                if r[x[1]] == -1 or x[1]+w < r[v[1]]:
                     q.put((v[1], x[1]+w, set([v[1]]) | x[2]))
             elif v[1] == x[0] and v[0] not in x[2]:
-                if r[v[0]] == -1 or x[0]+w < r[x[1]]:
+                if r[x[0]] == -1 or x[0]+w < r[v[0]]:
                     q.put((v[0], x[1]+w, set([v[0]]) | x[2]))
         print(q.qsize())
 
 
 def bfsV2():
     q = Queue()
-    q.put((s, 0, set([s])))
+    q.put((s, 0))
+    vis = set()
 
     while not q.empty():
         x = q.get()
         if r[x[0]] == -1 or x[1] < r[x[0]]:
             r[x[0]] = x[1]
-        if x[1] > r[x[0]]:
-            continue
         for v in c:
-            if v[0] == x[0] and v[1] not in x[2]:
-                if r[x[1]] == -1 or x[1]+w < r[x[1]]:
-                    q.put((v[1], x[1]+w, set([v[1]]) | x[2]))
-            elif v[1] == x[0] and v[0] not in x[2]:
-                if r[v[0]] == -1 or x[0]+w < r[x[1]]:
-                    q.put((v[0], x[1]+w, set([v[0]]) | x[2]))
-        print(q.qsize())
+            if v[0] == x[0] and v[1] not in vis and \
+                    (r[v[1]] == -1 or x[1]+w < r[v[1]]):
+                vis.add(v[1])
+                q.put((v[1], x[1]+w))
+            elif v[1] == x[0] and v[0] not in vis and \
+                    (r[v[0]] == -1 or x[1]+w < r[v[0]]):
+                vis.add(v[0])
+                q.put((v[0], x[1]+w))
 
 
 if __name__ == "__main__":
@@ -47,8 +47,8 @@ if __name__ == "__main__":
 
     for _ in range(t):
         n, m = map(int, input().split())
-        c = [list(int(v) for v in input().split()) for j in range(m)]
+        c = set(tuple(int(v) for v in input().split()) for j in range(m))
         s = int(input())
         r = [-1 for j in range(n+1)]
-        bfs()
+        bfsV2()
         print(" ".join(str(r[i]) for i in range(1, n+1) if i != s))
