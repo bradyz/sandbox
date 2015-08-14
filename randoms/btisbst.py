06b325dc0cab046b4b3d90e6d5c777dd58d36720
bradyz <brady.zhou@utexas.edu>
2015-08-13 21:26:42 -0400

    randoms binary tree is bst V2

--- randoms/btisbst.py
+++ randoms/btisbst.py
@@ -15,8 +15,33 @@ def isBST(r, minVal=-sys.maxsize, maxVal=sys.maxsize):
         return False
     return isBST(r.left, minVal, r.val) and isBST(r.right, r.val, maxVal)
 
+
+def isBSTV2(r):
+    def inorder():
+        s = [r]
+        while s:
+            cur = s.pop()
+            if cur:
+                s.append(cur.right)
+                s.append(cur)
+                s.append(cur.left)
+            else:
+                if s:
+                    yield s.pop().val
+
+    prev = None
+    for v in inorder():
+        if prev and v < prev:
+            return False
+        prev = v
+    return True
+
+
 isYes = Node(4, Node(2, Node(1), Node(3)), Node(5))
 isNot = Node(3, Node(2, Node(1), Node(4)), Node(5))
 
 print(isBST(isYes))
 print(isBST(isNot))
+
+print(isBSTV2(isYes))
+print(isBSTV2(isNot))
