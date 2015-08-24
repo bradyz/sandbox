d02243559218c39c52ee02598910f7906a21cb99
bradyz <brady.zhou@utexas.edu>
2015-08-24 18:47:51 -0400

    webdev react webpack tutorial

---
+++ webdev/reactjs/webpack-tutorial/webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+// could use export default **object**
+module.exports = {  
+  entry: [
+    'webpack/hot/only-dev-server',
+    './js/app.js'
+  ],
+
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
+};
