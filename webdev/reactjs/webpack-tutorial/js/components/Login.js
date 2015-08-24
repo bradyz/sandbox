f8665e6b0834acada6a00629fd367a49419c2b1d
bradyz <brady.zhou@utexas.edu>
2015-08-24 18:47:51 -0400

    webdev react webpack tutorial

---
+++ webdev/reactjs/webpack-tutorial/js/components/Login.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var Login = React.createClass({
+  render: function () {
+    return (
+      
+      <div>Welcome to login</div>
+
+    );
+  }
+});
+
+module.exports.Login = Login;
