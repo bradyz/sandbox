3ffcf965497022752be4f42b057b1f9797220825
bradyz <brady.zhou@utexas.edu>
2015-08-27 09:36:32 -0500

    geeksforgeeks same contacts O(n^2) dfs

--- randoms/samecontacts.py
+++ randoms/samecontacts.py
@@ -1,4 +1,4 @@
-# O(n^3) because the list is turned into a set
+# O(n^3) as the list is turned into a set
 def solve(contact_list):
     children = {i: [] for i in range(len(contact_list))}
     is_parent = [False for _ in range(len(contact_list))]
@@ -17,6 +17,45 @@ def solve(contact_list):
         print(" ".join(map(str, [parent] + children[parent])))
 
 
+# O(n^2) construction of adj_graph, O(V + E) for dfs
+def solveV2(contact_list):
+    def dfs(cur, parent):
+        if used[cur]:
+            return
+
+        group[parent].append(cur)
+        used[cur] = True
+
+        for head in range(len(adj_graph)):
+            if adj_graph[cur][head]:
+                dfs(head, parent)
+
+    adj_graph = [[False for j in contact_list] for i in contact_list]
+    group = {i: [] for i in range(len(contact_list))}
+    used = [False for _ in contact_list]
+
+    for i in range(len(contact_list)):
+        for j in range(i+1, len(contact_list)):
+            if contact_list[i][0] == contact_list[j][0] or \
+                    contact_list[i][0] == contact_list[j][1] or \
+                    contact_list[i][0] == contact_list[j][2] or \
+                    contact_list[i][1] == contact_list[j][0] or \
+                    contact_list[i][1] == contact_list[j][1] or \
+                    contact_list[i][1] == contact_list[j][2] or \
+                    contact_list[i][2] == contact_list[j][0] or \
+                    contact_list[i][2] == contact_list[j][1] or \
+                    contact_list[i][2] == contact_list[j][2]:
+                adj_graph[i][j] = True
+                adj_graph[j][i] = True
+
+    for i in range(len(contact_list)):
+        dfs(i, i)
+
+    for people in (g for g in group if group[g]):
+        print(" ".join(map(str, group[people])))
+
+
 if __name__ == "__main__":
     contacts = [tuple(input().split()) for _ in range(int(input()))]
+    solveV2(contacts)
     solve(contacts)
