0c19a16b7ce176aa27a1e317ea49a8269c50d0d0
bradyz <brady.zhou@utexas.edu>
2015-08-28 01:29:57 -0500

    randoms anagram using xor

--- randoms/anagram.py
+++ randoms/anagram.py
@@ -1,23 +1,29 @@
-from collections import Counter
-
-
 def is_anagramA(a, b):
     return sorted(str(a)) == sorted(str(b))
 
 
 def is_anagramB(a, b):
+    from collections import Counter
     return Counter(a) == Counter(b)
+
+
+def is_anagramC(a, b):
+    xor = 0
+    for u, v in zip(a, b):
+        xor ^= ord(u) ^ ord(v)
+    return xor == 0
 
 if __name__ == "__main__":
     lines = []
+
     with open('anagram-input.txt', 'r') as f:
         lines = f.readlines()
+
+    funcs = [is_anagramA, is_anagramB, is_anagramC]
 
     for x, n in enumerate(lines):
         if x % 2 == 0:
             strA = n.strip("\n")
         else:
             strB = n.strip("\n")
-            print(strA, strB)
-            print("Sort: " + str(is_anagramA(strA, strB)))
-            print("Dict: " + str(is_anagramB(strA, strB)))
+            print("\n".join(map(lambda x: str(x(strA, strB)), funcs)))
