8867f71b93a429ccbedb55ae92958ad316ef0304
bradyz <brady.zhou@utexas.edu>
2015-10-15 10:34:25 -0500

    randoms words of words better

--- randoms/wordofwords.py
+++ randoms/wordofwords.py
@@ -8,8 +8,24 @@ def can_form(rest):
                if rest[:i] in dictionary)
 
 
+def can_formV2(rest):
+    if rest in dictionary:
+        return True
+
+    for i in range(len(rest)+1):
+        if rest[:i] in dictionary:
+            if can_formV2(rest[i:]):
+                return True
+
+    return False
+
+
 dictionary = [input() for _ in range(int(input()))]
 
 print(can_form("hellocat"))
 print(can_form("hellcat"))
 print(can_form("hellcate"))
+
+print(can_formV2("hellocat"))
+print(can_formV2("hellcat"))
+print(can_formV2("hellcate"))
