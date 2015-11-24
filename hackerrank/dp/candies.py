92bf8a808b32b4d89cfd8bf1047694402f20429f
bradyz <brady.zhou@utexas.edu>
2015-11-24 15:03:53 -0600

    hackerrank candies dp

--- hackerrank/dp/candies.py
+++ hackerrank/dp/candies.py
@@ -3,11 +3,8 @@ c = [int(input()) for _ in range(n)]
 dp = [1 for _ in range(n)]
 for i in range(1, n):
     if c[i] > c[i-1]:
-        dp[i] += dp[i-1]
-    else:
-        dp[i] = dp[i-1] - 1
+        dp[i] = dp[i-1]+1
 for i in range(n-2, -1, -1):
-    if dp[i] > dp[i+1]:
-        dp[i+1] = max(1, dp[i+1] - 1)
-print(dp)
+    if c[i] > c[i+1]:
+        dp[i] = max(dp[i+1]+1, dp[i])
 print(sum(dp))
