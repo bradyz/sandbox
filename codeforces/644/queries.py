b71e24cac9872b5253a88f46a7821bab2d6b32a4
bradyz <brady.zhou@utexas.edu>
2016-03-18 11:16:32 -0500

    codeforces 644 div 2 b queries priority queue fix TLE

--- codeforces/644/queries.py
+++ codeforces/644/queries.py
@@ -1,19 +1,19 @@
-from queue import PriorityQueue
+from heapq import heappush, heappop
 from collections import deque
 
 
 number_jobs, queue_size = map(int, input().split())
 
 ret = dict()
-events = PriorityQueue()
+events = list()
 jobs = deque()
 
 for i in range(number_jobs):
     time, duration = map(int, input().split())
-    events.put((time, 1, i, duration))
+    heappush(events, (time, 1, i, duration))
 
-while not events.empty():
-    data = events.get()
+while events:
+    data = heappop(events)
 
     if data[1] == 0:
         finish_time, job_number = data[0], data[2]
@@ -23,13 +23,13 @@ while not events.empty():
         if jobs:
             job, duration = jobs.popleft()
             jobs.appendleft((job, duration))
-            events.put((finish_time + duration, 0, job))
+            heappush(events, (finish_time + duration, 0, job))
     else:
         time, job_number, duration = data[0], data[2], data[3]
 
         if len(jobs) <= queue_size:
             if not jobs:
-                events.put((time + duration, 0, job_number))
+                heappush(events, (time + duration, 0, job_number))
             jobs.append((job_number, duration))
         else:
             ret[job_number] = -1
