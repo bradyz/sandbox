d6efd15a0904ae678d07efbd00492da61da8939b
bradyz <brady.zhou@utexas.edu>
2016-04-04 21:44:37 -0500

    ut acm i read the problem wrong

--- challenges/ut-acm-4-1/doodle.py
+++ challenges/ut-acm-4-1/doodle.py
@@ -5,11 +5,9 @@ for _ in range(int(input())):
     r = [list(map(int, input().split())) for _ in range(n)]
     a = {atan2(y, x): (x, y) for y, x in r}
     s = list(sorted(a.keys()))
-    ret = 1e9
-    print(a)
+    ret = 0
     for i in range(n):
-        print(i, (i-1) % n)
         d = (a[s[i]][0] - a[s[(i-1) % n]][0]) ** 2 + \
             (a[s[i]][1] - a[s[(i-1) % n]][1]) ** 2
-        ret = min(ret, d)
+        ret = max(ret, d)
     print("%.3f" % sqrt(ret))
