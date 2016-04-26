e44643e7a1e15bdd9ba9153887a1d06755eb2313
bradyz <brady.zhou@utexas.edu>
2016-04-25 20:36:49 -0500

    algorithms segment tree update range

--- algorithms/segmenttree.py
+++ algorithms/segmenttree.py
@@ -32,11 +32,32 @@ def query(i, a, b, l, r):
     return min(lhs, rhs)
 
 
+def update(i, a, b, l, r, v):
+    if b < a:
+        return
+    elif b < l or r < a:
+        return
+    elif a == b:
+        tree[i] += v
+        return
+    update(2 * i + 1, a, (a + b) // 2, l, r, v)
+    update(2 * i + 2, (a + b) // 2 + 1, b, l, r, v)
+    tree[i] = min(tree[2 * i + 1], tree[2 * i + 2])
+
+
 if __name__ == "__main__":
     val = [randrange(1, 100) for _ in range(4)]
     n = len(val)
+
     build(0, 0, n - 1)
     print(val)
+    print(tree[:2*n-1])
+    for i in range(n):
+        for j in range(i, n):
+            print(i, j, query(0, 0, n-1, i, j))
+
+    update(0, 0, n-1, 1, 3, 5)
+    print("updated")
     print(tree[:2*n-1])
     for i in range(n):
         for j in range(i, n):
