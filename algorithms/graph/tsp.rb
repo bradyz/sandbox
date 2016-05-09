4d4809a4246d183c3db05e176eb726a80d6f92b4
bradyz <brady.zhou@utexas.edu>
2016-05-08 22:37:01 -0500

    fix tsp almost done

--- algorithms/graph/tsp.rb
+++ algorithms/graph/tsp.rb
@@ -1,100 +1,123 @@
 require 'set'
 
+Edge = Struct.new(:u, :v, :w)
+Node = Struct.new(:x, :y, :n)
 
-def has_cycle(the_graph, start)
-  visited = Set.new [start]
+def has_cycle(the_graph, start, visited)
+  visited.add(start)
   stack = [start]
+
+  puts the_graph.length
 
   while stack.length > 0
     u = stack.pop
-    
-    new_visited = visited
 
     the_graph.each do |edge|
       if edge.u == u
         if visited.include?(edge.v)
           return true
-        else
-          new_visited.add(edge.v)
-          stack.push(edge.v)
         end
+
+        visited.add(edge.v)
+        stack.push(edge.v)
       end
     end
-
-    visited = new_visited
   end
 
   return false
 end
 
-Edge = Struct.new(:u, :v, :w)
-graph = []
-nodes = Set.new
-
-STDIN.each_with_index do |line, idx|
-  line_split = line.split()
-  begin
-    line_split.map! { |x| Integer(x) }
-    tmp = Edge.new
-    tmp.u, tmp.v, tmp.w = line_split
-    graph.push(tmp)
-
-    nodes.add(tmp.u)
-    nodes.add(tmp.v)
-  rescue
-  end
+def dist(a, b)
+  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
 end
 
-graph.sort! { |a, b| a[:w] <=> b[:w] }
+def main()
+  graph = []
+  nodes = []
 
-degree = {}
-solution = []
-
-graph.each do |edge|
-  if !degree.include?(edge.u)
-    degree[edge.u] = 0
-  end
-  if !degree.include?(edge.v)
-    degree[edge.v] = 0
-  end
-  if degree[edge.u] == 2
-    next
-  end
-  if degree[edge.v] == 2
-    next
-  end
-
-  tmp_graph = solution + [edge]
-
-  cycle = false
-
-  tmp_graph.each do |e|
-    if has_cycle(tmp_graph, e.u)
-      cycle = true
+  STDIN.each_with_index do |line, idx|
+    line_split = line.split()
+    begin
+      line_split.map! { |x| Integer(x) }
+      tmp = Node.new
+      tmp.n, tmp.x, tmp.y = line_split
+      nodes.push(tmp)
+    rescue
     end
   end
 
-  if cycle
-   if tmp_graph.length == nodes.length
-     puts "found"
-     solution = tmp_graph
-     break
-   end
+  nodes.each do |a|
+    nodes.each do |b|
+      if a == b
+        next
+      end
+
+      graph.push(Edge.new(a, b, dist(a, b)))
+      graph.push(Edge.new(b, a, dist(a, b)))
+    end
   end
 
-  if !cycle
-    solution = tmp_graph
-    degree[edge.u] += 1
-    degree[edge.v] += 1
+  graph.sort! { |a, b| a[:w] <=> b[:w] }
+
+  degree = {}
+  solution = []
+
+  puts graph.length
+
+  graph.each do |edge|
+    if !degree.include?(edge.u)
+      degree[edge.u] = 0
+    end
+    if !degree.include?(edge.v)
+      degree[edge.v] = 0
+    end
+    if degree[edge.u] >= 2
+      next
+    end
+    if degree[edge.v] >= 2
+      next
+    end
+
+    tmp_graph = solution + [edge]
+
+    cycle = false
+
+    visited = Set.new
+
+    tmp_graph.each do |e|
+      if visited.include?(e.u)
+        next
+      end
+
+      if has_cycle(tmp_graph, e.u, visited)
+        cycle = true
+      end
+    end
+
+    if cycle
+     if tmp_graph.length == nodes.length
+       puts "found"
+       solution = tmp_graph
+       break
+     end
+    end
+
+    if !cycle
+      solution = tmp_graph
+      degree[edge.u] += 1
+      degree[edge.v] += 1
+    end
   end
+
+  total = 0
+
+  solution.each do |e|
+    puts e.u.n, e.v.n
+    total += Math.sqrt(e.w)
+  end
+
+  puts "return"
+  puts solution.length, total
 end
 
-total = 0
-
-solution.each do |e|
-  total += e.w
-end
-
-solution.sort! { |a, b| a[:u] <=> b[:u] }
-puts solution
-puts solution.length, total
+main()
