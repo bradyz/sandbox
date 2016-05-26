51f7a06d02826b01688679ab361b8f2a8b0210c2
bradyz <brady.zhou@utexas.edu>
2016-05-25 23:29:09 -0400

    codeforces 671 div 1 a bottles another WA

--- codeforces/671/bottles.py
+++ codeforces/671/bottles.py
@@ -9,35 +9,58 @@ bdist = list()
 for i in range(n):
     x, y = map(int, input().split())
     dist.append((tx - x) ** 2 + (ty - y) ** 2)
-    if dist[-1] + (ax - x) ** 2 + (ay - y) ** 2 < dist[-1] * 2:
-        adist.append((dist[-1] + (ax - x) ** 2 + (ay - y) ** 2, i))
-    if dist[-1] + (bx - x) ** 2 + (by - y) ** 2 < dist[-1] * 2:
-        bdist.append((dist[-1] + (bx - x) ** 2 + (by - y) ** 2, i))
-found = [False for _ in range(n)]
-adist.sort()
-bdist.sort()
-besta = 0
-bestb = 0
-if len(adist) == 0:
-    besta = -1
-if len(bdist) == 0:
-    bestb = -1
-while besta >= 0 and bestb >= 0 and adist[besta][1] == bdist[bestb][1]:
-    if adist[besta][0] < bdist[bestb][0]:
-        bestb += 1
-    else:
-        besta += 1
-print(adist, besta)
-print(bdist, bestb)
-print(dist)
+    # print(dist[i], (ax - x) ** 2 + (ay - y) ** 2)
+    adist.append((dist[i] - ((ax - x) ** 2 + (ay - y) ** 2), i))
+    bdist.append((dist[i] - ((bx - x) ** 2 + (by - y) ** 2), i))
+adist.sort(reverse=True)
+bdist.sort(reverse=True)
 ret = 0
-ret += adist[besta][0]
+if adist[0][0] > 0 and bdist[0][0] > 0:
+    used = set()
+    if adist[0][1] == bdist[0][1]:
+        if n > 1:
+            if adist[0][0] + bdist[1][0] > adist[1][0] + bdist[0][0]:
+                used.add(adist[0][1])
+                used.add(bdist[1][1])
+                ret += sqrt(dist[adist[0][1]] - adist[0][0])
+                ret += sqrt(dist[bdist[1][1]] - bdist[1][0])
+            else:
+                used.add(adist[1][1])
+                used.add(bdist[0][1])
+                ret += sqrt(dist[adist[1][1]] - adist[1][0])
+                ret += sqrt(dist[bdist[0][1]] - bdist[0][0])
+        else:
+            if adist[0][0] > bdist[0][0]:
+                used.add(adist[0][1])
+                ret += sqrt(dist[adist[0][1]] - adist[0][0])
+            else:
+                used.add(bdist[0][1])
+                ret += sqrt(dist[bdist[0][1]] - bdist[0][0])
+    else:
+        used.add(adist[0][1])
+        used.add(bdist[0][1])
+        ret += sqrt(dist[adist[0][1]] - adist[0][0])
+        ret += sqrt(dist[bdist[0][1]] - bdist[0][0])
+    # print(used, ret)
+    for i in range(n):
+        if i in used:
+            ret += sqrt(dist[i])
+            continue
+        ret += 2 * sqrt(dist[i])
+else:
+    used = set()
+    if adist[0][0] < bdist[0][0]:
+        used.add(adist[0][1])
+        ret += sqrt(dist[adist[0][1]] - adist[0][0])
+    else:
+        used.add(bdist[0][1])
+        ret += sqrt(dist[bdist[0][1]] - bdist[0][0])
+    for i in range(n):
+        if i in used:
+            ret += sqrt(dist[i])
+            continue
+        ret += 2 * sqrt(dist[i])
+print(dist)
+print(adist[:2])
+print(bdist[:2])
 print(ret)
-ret += bdist[bestb][0]
-print(ret)
-for i in range(n):
-    if i == adist[besta][1] or i == bdist[bestb][1]:
-        continue
-    print(i)
-    ret += dist[i] * 2
-print(sqrt(ret))
