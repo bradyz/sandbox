d65c264edb29adbf95efdab0331da52312b3f619
bradyz <brady.zhou@utexas.edu>
2016-07-01 23:47:17 -0400

    leetcode jump game implementation linear

--- leetcode/jumpgame.py
+++ leetcode/jumpgame.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def canJump(self, nums):
+    def canJumpTLE(self, nums):
         dp = [False for _ in nums]
         dp[0] = True
         for i in range(1, len(nums)):
@@ -8,12 +8,31 @@ class Solution(object):
                     dp[i] = True
         return dp[-1]
 
+    def canJump(self, nums):
+        c = 0
+        while c < len(nums) - 1:
+            if c + nums[c] >= len(nums) - 1:
+                return True
+            m = 0
+            x = 0
+            j = c
+            for i in range(c+1, min(c+nums[c]+1, len(nums))):
+                if nums[i] + x > m:
+                    m = nums[i] + x
+                    j = i
+                x += 1
+            if m == 0:
+                return False
+            c = j
+        return True
 
 
 a = [2, 3, 1, 1, 4]
 b = [3, 2, 1, 0, 4]
 c = [0]
+d = [1, 2, 0, 1]
 s = Solution()
 print(s.canJump(a))
 print(s.canJump(b))
 print(s.canJump(c))
+print(s.canJump(d))
