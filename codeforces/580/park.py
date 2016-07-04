77a8d84f116c479a20c0abcb325d4541bdcd7dfa
bradyz <brady.zhou@utexas.edu>
2016-07-03 20:49:49 -0400

    codeforces 580 div 2 c park dfs

--- codeforces/580/park.py
+++ codeforces/580/park.py
@@ -1,21 +1,27 @@
-def solve():
-    def dfs(x, y):
-        s.add(x)
-        if y > m:
-            return 0
-        if not g[x] - s:
-            return 1
-        return sum(dfs(v, (c[v] == 1 and y)+c[v]) for v in g[x] if v not in s)
-
-    s = set()
-    print(dfs(1, c[1]))
-
-if __name__ == "__main__":
-    n, m = map(int, input().split())
-    c = [0] + list(map(int, input().split()))
-    g = {i: set() for i in range(1, n+1)}
-    for _ in range(n-1):
-        u, v = map(int, input().split())
-        g[u].add(v)
-        g[v].add(u)
-    solve()
+n, m = map(int, input().split())
+c = [0] + list(map(int, input().split()))
+g = {i: list() for i in range(1, n+1)}
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    g[u].append(v)
+    g[v].append(u)
+r = 0
+vis = set([1])
+s = [(1, c[1])]
+while s:
+    u, w = s.pop()
+    if w > m:
+        continue
+    found = False
+    for v in g[u]:
+        if v in vis:
+            continue
+        vis.add(v)
+        found = True
+        if c[v] == 0:
+            s.append((v, 0))
+        else:
+            s.append((v, w + c[v]))
+    if not found:
+        r += 1
+print(r)
