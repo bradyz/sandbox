eba7402b6ecff4b6003aecbb340c65917adf6771
bradyz <brady.zhou@utexas.edu>
2016-08-12 17:49:20 -0400

    codeforces 706 div 2 c hard dp fix bug

--- codeforces/706/hard.cpp
+++ codeforces/706/hard.cpp
@@ -6,18 +6,19 @@
 using namespace std;
 
 int n;
-int c[100005];
+unsigned long long c[100005];
 string s[100005][2];
 bool can[100005][2];
-int dp[100005][2];
+unsigned long long dp[100005][2];
 
 int main () {
-    memset(c, 0, sizeof c);
-    memset(dp, int(1e9), sizeof dp);
-    memset(can, 0, sizeof can);
     cin >> n;
     for (int i = 0; i < n; ++i) {
         cin >> c[i];
+        can[i][0] = false;
+        can[i][1] = false;
+        dp[i][0] = (unsigned long long)(1e20);
+        dp[i][1] = (unsigned long long)(1e20);
     }
     for (int i = 0; i < n; ++i) {
         cin >> s[i][0];
@@ -32,21 +33,27 @@ int main () {
         if (!can[i-1][0] && !can[i-1][1])
             break;
         if (can[i-1][0]) {
-            if (s[i][0] > s[i-1][0])
+            if (s[i][0] >= s[i-1][0]) {
                 dp[i][0] = min(dp[i][0], dp[i-1][0]);
-            if (s[i][1] > s[i-1][0])
-                dp[i][0] = min(dp[i][0], dp[i-1][0] + c[i]);
+                can[i][0] = true;
+            }
+            if (s[i][1] >= s[i-1][0]) {
+                dp[i][1] = min(dp[i][1], dp[i-1][0] + c[i]);
+                can[i][1] = true;
+            }
         }
         if (can[i-1][1]) {
-            if (s[i][0] > s[i-1][1])
-                dp[i][1] = min(dp[i][1], dp[i-1][1]);
-            if (s[i][1] > s[i-1][1])
+            if (s[i][0] >= s[i-1][1]) {
+                dp[i][0] = min(dp[i][0], dp[i-1][1]);
+                can[i][0] = true;
+            }
+            if (s[i][1] >= s[i-1][1]) {
                 dp[i][1] = min(dp[i][1], dp[i-1][1] + c[i]);
+                can[i][1] = true;
+            }
         }
     }
-    for (int i = 0 ; i < n; ++i)
-        cout << dp[i][0] << " " << dp[i][1] << endl;
-    if (can[n-1][0] || can[n-1][2])
+    if (can[n-1][0] || can[n-1][1])
         cout << min(dp[n-1][0], dp[n-1][1]) << endl;
     else
         cout << -1 << endl;
