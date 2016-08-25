a59dd86843f746213e496b5f4a971ba9e18f53da
bradyz <brady.zhou@utexas.edu>
2016-08-25 14:59:36 -0500

    fix up drunken cycle detection

--- algorithms/cycledetection.py
+++ algorithms/cycledetection.py
@@ -1,12 +1,12 @@
 def search(u, g, vis, done):
     vis.add(u)
     for v in g.get(u, []):
-        if v not in vis:
-            return search(v, vis)
-        elif not done[v]:
-            return False
-    done.add(v)
-    return True
+        if v not in vis and search(v, g, vis, done):
+            return True
+        elif v not in done:
+            return True
+    done.add(u)
+    return False
 
 
 def has_cycle(g):
@@ -18,3 +18,14 @@ def has_cycle(g):
         if search(u, g, vis, done):
             return True
     return False
+
+
+if __name__ == "__main__":
+    e = int(input())
+    g = dict()
+    for _ in range(e):
+        u, v = map(int, input().split())
+        if u not in g:
+            g[u] = list()
+        g[u].append(v)
+    print(has_cycle(g))
