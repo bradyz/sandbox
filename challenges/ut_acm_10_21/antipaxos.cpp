6d1f4909691c490150c1c14901442299d2249ce2
bradyz <brady.zhou@utexas.edu>
2016-10-24 00:52:38 -0500

    ut acm antipaxos connected component strings are slow

--- challenges/ut_acm_10_21/antipaxos.cpp
+++ challenges/ut_acm_10_21/antipaxos.cpp
@@ -1,100 +1,65 @@
+#include <cstdio>
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
+#include <unordered_set>
+#include <unordered_map>
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int T;
 int n;
-map<string, vector<string> > g;
+unordered_map<int, vector<int> > g;
+unordered_map<string, int> l;
+unordered_set<int> v;
 
-void print (map<string, vector<string> > g) {
-    for (auto it : g) {
-        cout << it.first << ": ";
-        for (auto it1 : it.second) {
-            cout << it1 << " ";
-        }
-        cout << endl;
-    }
-}
-
-void dfs (string u, set<string>& v, map<string, vector<string> >& g, vector<string>& o) {
+int dfs (int u) {
     if (v.find(u) != v.end())
-        return;
+        return 0;
     v.insert(u);
-    for (string to : g[u]) {
-        dfs(to, v, g, o);
+    int r = 1;
+    for (int d : g[u]) {
+        r += dfs(d);
     }
-    o.push_back(u);
+    return r;
 }
 
 void solve () {
-    set<string> v;
-    vector<string> r;
-    map<string, vector<string> >::iterator it;
-    for (it = g.begin(); it != g.end(); ++it) {
-        dfs(it->first, v, g, r);
+    int r = int(1e9);
+    for (auto it : g) {
+        v.clear();
+        r = min(r, dfs(it.first));
     }
-    reverse(r.begin(), r.end());
-
-
-    map<string, vector<string> > gr;
-    set<string> v1;
-
-    for (it = g.begin(); it != g.end(); ++it) {
-        if (gr.find(it->first) == gr.end())
-            gr[it->first] = vector<string>();
-        for (string s : it->second) {
-            if (gr.find(s) == gr.end())
-                gr[s] = vector<string>();
-            gr[s].push_back(it->first);
-        }
-    }
-
-    // print(g);
-    // print(gr);
-    vector<string> ans;
-    for (string u : r) {
-        vector<string> scc;
-        dfs(u, v1, gr, scc);
-        if (scc.size() > 0) {
-            ans = scc;
-        }
-    }
-
-    cout << ans.size() << endl;
-    //
-    // for (vector<string> scc : sccs) {
-    //     for (string s : scc)
-    //         cout << s << " ";
-    //     cout << endl;
-    // }
+    cout << r << endl;
 }
 
 int main () {
+    ios::sync_with_stdio(0);
     cin >> T;
     while (T--) {
         g.clear();
-
+        l.clear();
         cin >> n;
-
         string s, u, v;
         for (int i = 0; i <= n; ++i) {
             getline(cin, s);
-            if (i == 0)
-                continue;
+            if (i == 0) continue;
             stringstream ss(s);
             ss >> u;
-            g[u] = vector<string>();
+            if (l.find(u) == l.end())
+                l[u] = l.size();
+            int x = l[u];
+            g[x] = vector<int>();
             while (ss >> v) {
-                g[u].push_back(v);
+                if (l.find(v) == l.end())
+                    l[v] = l.size();
+                int y = l[v];
+                g[x].push_back(y);
             }
         }
-
         solve();
     }
     return 0;
