5dd68ea759a42f197f63929a2af92cd1d8e8fad6
bradyz <brady.zhou@utexas.edu>
2016-11-04 00:38:51 -0500

    hackerrank game dp binary search TLE

--- hackerrank/dp/game.py
+++ hackerrank/dp/game.py
@@ -1,18 +1,23 @@
+import sys
 from bisect import bisect_left
+
+sys.setrecursionlimit(10000)
 
 
 def recur(prefix, lo, hi, memo, arr):
-    if (lo, hi) in memo:
+    if hi <= lo:
+        return 0
+    elif (lo, hi) in memo:
         return memo[(lo, hi)]
     val = prefix[hi] - prefix[lo]
-    if val % 2 != 0:
-        memo[(lo, hi)] = 0
-        return 0
-    i = bisect_left(prefix, prefix[lo] + val // 2, lo, hi) + 1
-    if i >= hi:
-        return 0
-    print(lo, hi, i, c[lo:i], c[i:hi+1])
-    result = max(recur(prefix, lo, i, memo, arr), recur(prefix, i-1, hi, memo, arr)) + 1
+    i = bisect_left(prefix, prefix[lo] + val // 2, lo, hi)
+    if i == lo:
+        i += 1
+    result = 0
+    while i < hi and prefix[i] - prefix[lo] == prefix[hi] - prefix[i]:
+        result = max(result, recur(prefix, lo, i, memo, arr) + 1)
+        result = max(result, recur(prefix, i, hi, memo, arr) + 1)
+        i += 1
     memo[(lo, hi)] = result
     return result
 
@@ -21,7 +26,6 @@ def solve(c, n):
     prefix = list(c)
     for i in range(1, n):
         prefix[i] += prefix[i-1]
-    print(prefix)
     print(recur(prefix, 0, n-1, dict(), c))
 
 
