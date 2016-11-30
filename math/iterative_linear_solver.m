4f9ceaf45c7714aaa8c91b11afc55a6118cc115c
bradyz <brady.zhou@utexas.edu>
2016-11-29 20:13:57 -0600

    add linear solver for SPD matrices

--- math/iterative_linear_solver.m
+++ math/iterative_linear_solver.m
@@ -1,38 +1,35 @@
 % This linear solver takes the inspiration from Euler's method for solving
 % differential equations.
 %
-% We want to solve Ax = b, and we are given A in R^{m, n} and b in R^{m}.
+% We want to solve Ax = b.
+% A in m x m, symmetric positive definite (done by B' * B for B full rank).
+% x in n x 1.
+% b in n x 1.
 % The goal is to find an x in R^{n} such that ||Ax - b|| is minimized.
-%
-% The following scheme is applied -
-% x_0 = A^T * b.
-% r_k = (beta * I + A^T * A) * x - A^T * b.
-% x_{k+1} = x_k + delta * r_k.
-% And iterate until ||x_{k+1} - x_k|| < epsilon.
 
 % Metadata.
 m = 5;
 n = 3;
-eps = 1e-5;
-delta = 1e-6;
-alpha = 1e-10;
+eps = 1e-10;
+delta = 1e-2;
 
 % Want to solve: Ax = b.
-A = randn(m, n);
-b = randn(m, 1);
+A = rand(m, n);
+A = A' * A;
+b = rand(n, 1);
 
-% Save some intermediate calculations.
-beta_I = alpha * eye(n);
-AT_A = A' * A;
-AT_b = A' * b;
+% Set initial guess.
+tmp = 0;
+x_k = b;
+k = 1;
 
-% Intialize guesses.
-x_a = AT_b;
-x_b = x_a + delta * ((beta_I + AT_A) * x_a - AT_b);
-
-% Until convergence, step.
-while norm(x_b - x_a) > eps
-    norm(x_b - x_a)
-    x_a = x_b;
-    x_b = x_a + delta * ((beta_I + AT_A) * x_a - AT_b);
+while norm(x_k - tmp) > eps * norm(b)
+    % tmp is x_k-1.
+    tmp = x_k;
+    x_k = tmp + delta * (b - A * tmp);
+    k = k + 1;
 end
+
+% Took k iterations.
+k
+norm(A * x_k - b)
