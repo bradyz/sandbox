9627dc6c4db18c596e089e2ad18c73e507aa3413
bradyz <brady.zhou@utexas.edu>
2017-01-25 02:34:38 -0600

    cs395t more code for checking answers

--- math/cs395t_hw1.m
+++ math/cs395t_hw1.m
@@ -19,10 +19,12 @@ f_1 = .5 * q' * A * q;
 f_2 = .5 * (q + s * w)' * A * (q + s * w);
 
 % Numerical solution.
-r_1d = (f_2 - f_1) ./ s
+r_1d = (f_2 - f_1) ./ s;
 
 % Analytic solution.
-a_1d = q' * A * w
+a_1d = q' * A * w;
+
+fprintf('Problem 1d: %0.10f\n', norm(r_1d - a_1d) / norm(r_1d));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Problem 2a.
@@ -39,10 +41,12 @@ f_1 = norm(A * q - b)^2;
 f_2 = norm(A * (q + s * w) - b)^2;
 
 % Numerical solution.
-r_2a = (f_2 - f_1) / s
+r_2a = (f_2 - f_1) / s;
 
 % Analytic solution.
-a_2a = 2 * (A * q - b)' * (A * w)
+a_2a = 2 * (A * q - b)' * (A * w);
+
+fprintf('Problem 2a: %0.10f\n', norm(r_2a - a_2a) / norm(r_2a));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Problem 2b.
@@ -57,10 +61,37 @@ f_1 = trace(A);
 f_2 = trace(A + s * B);
 
 % Numerical solution.
-r_2b = (f_2 - f_1) ./ s
+r_2b = (f_2 - f_1) ./ s;
 
 % Analytic solution.
-a_2b = trace(B)
+a_2b = trace(B);
+
+fprintf('Problem 2d: %0.10f\n', norm(r_2b - a_2b) / norm(r_2b));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 2c.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+s = 1e-2;
+
+q = randn(3, 1) * 1e4;
+w = randn(3, 1);
+
+% Numerical solution.
+u = @fu;
+v = @fv;
+du = @dfu;
+dv = @dfv;
+
+f_1 = norm(cross(u(q), v(q)));
+f_2 = norm(cross(u(q + s * w), v(q + s * w)));
+r_2c = (f_2 - f_1) ./ s;
+
+% Analytic solution.
+a = cross(u(q), v(q))' * (cross(du(q, s, w), v(q)) + cross(u(q), dv(q, s, w)));
+b = norm(cross(u(q), v(q)));
+a_2c = a / b;
+
+fprintf('Problem 2c: %0.10f\n', norm(r_2c - a_2c) / norm(r_2c));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Problem 2d.
@@ -78,9 +109,31 @@ f_1 = 1 / 6 * (cross(u - q, v - q))' * (w - q);
 f_2 = 1 / 6 * (cross(u - (q + s * dq), v - (q + s * dq)))' * (w - (q + s * dq));
 
 % Numerical solution.
-r_2d = (f_2 - f_1) / s
+r_2d = (f_2 - f_1) / s;
 
 % Analytic solution.
 a_2d = -1 / 6 * (cross(u - q, dq)'    * (w - q) + ...
                  cross(dq, v - q)'    * (w - q) + ...
-                 cross(u - q, v - q)' * (dq))
+                 cross(u - q, v - q)' * (dq));
+
+fprintf('Problem 2d: %0.10f\n', norm(r_2d - a_2d) / norm(r_2d));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Functions for Problem 2c.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function x = fu(q)
+    x = q / norm(q) + [0, 1, 0]';
+end
+
+function x = fv(q)
+    x = q / norm(q) + [1, 0, 0]';
+end
+
+function x = dfu(q, s, w)
+    x = (fu(q + s * w) - fu(q)) ./ s;
+end
+
+function x = dfv(q, s, w)
+    x = (fv(q + s * w) - fv(q)) ./ s;
+end
