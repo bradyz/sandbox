a764bc48fe14c4c8f106d155d461eb87d3bcc733
bradyz <brady.zhou@utexas.edu>
2017-09-08 15:01:44 -0500

    segment tree class

--- algorithms/segmenttree.py
+++ algorithms/segmenttree.py
@@ -1,64 +1,90 @@
-from random import randrange
-
-
 INF = 1e9
-N = 100000
-tree = [0 for _ in range(N)]
 
 
-# build over [a, b]
-def build(i, a, b):
-    if b < a:
-        return INF
-    elif a == b:
-        tree[i] = val[a]
-    else:
-        l = build(2 * i + 1, a, (a + b) // 2)
-        r = build(2 * i + 2, (a + b) // 2 + 1, b)
-        tree[i] = min(l, r)
-    return tree[i]
+class SegmentTree(object):
+    def __init__(self, values):
+        self.n = len(values)
+        self.values = values
+
+        self.tree = [0 for _ in range(2 * self.n - 1)]
+
+        self._build(0, 0, self.n-1)
+
+    def _build(self, i, a, b):
+        if b < a:
+            return INF
+
+        if a == b:
+            self.tree[i] = self.values[a]
+        else:
+            l = self._build(2 * i + 1, a, (a + b) // 2)
+            r = self._build(2 * i + 2, (a + b) // 2 + 1, b)
+
+            self.tree[i] = min(l, r)
+
+        return self.tree[i]
+
+    def query(self, l, r):
+        return self._query(0, 0, self.n-1, l, r)
+
+    def update(self, l, r, v):
+        return self._update(0, 0, self.n-1, l, r, v)
+
+    # query over [l, r]
+    def _query(self, i, a, b, l, r):
+        if b < a:
+            return INF
+        elif b < l or r < a:
+            return INF
+        elif a >= l and b <= r:
+            return self.tree[i]
+
+        lhs = self._query(2 * i + 1, a, (a + b) // 2, l, r)
+        rhs = self._query(2 * i + 2, (a + b) // 2 + 1, b, l, r)
+
+        return min(lhs, rhs)
+
+    def _update(self, i, a, b, l, r, v):
+        if b < a:
+            return
+        elif b < l or r < a:
+            return
+        elif a == b:
+            self.tree[i] += v
+            return
+
+        self._update(2 * i + 1, a, (a + b) // 2, l, r, v)
+        self._update(2 * i + 2, (a + b) // 2 + 1, b, l, r, v)
+
+        self.tree[i] = min(self.tree[2 * i + 1], self.tree[2 * i + 2])
 
 
-# query over [l, r]
-def query(i, a, b, l, r):
-    if b < a:
-        return INF
-    elif b < l or r < a:
-        return INF
-    elif a >= l and b <= r:
-        return tree[i]
-    lhs = query(2 * i + 1, a, (a + b) // 2, l, r)
-    rhs = query(2 * i + 2, (a + b) // 2 + 1, b, l, r)
-    return min(lhs, rhs)
+def test(val, tree):
+    result = [tree.query(i, i) for i in range(len(val))]
 
+    print(val)
+    print(result)
 
-def update(i, a, b, l, r, v):
-    if b < a:
-        return
-    elif b < l or r < a:
-        return
-    elif a == b:
-        tree[i] += v
-        return
-    update(2 * i + 1, a, (a + b) // 2, l, r, v)
-    update(2 * i + 2, (a + b) // 2 + 1, b, l, r, v)
-    tree[i] = min(tree[2 * i + 1], tree[2 * i + 2])
+    for i in range(len(val)):
+        for j in range(i, len(val)):
+            assert min(val[i:j+1]) == tree.query(i, j)
 
 
 if __name__ == "__main__":
-    val = [randrange(1, 100) for _ in range(4)]
-    n = len(val)
+    val = [100 for _ in range(8)]
+    tree = SegmentTree(val)
 
-    build(0, 0, n - 1)
-    print(val)
-    print(tree[:2*n-1])
-    for i in range(n):
-        for j in range(i, n):
-            print(i, j, query(0, 0, n-1, i, j))
+    # Vanilla test.
+    test(val, tree)
 
-    update(0, 0, n-1, 1, 3, 5)
-    print("updated")
-    print(tree[:2*n-1])
-    for i in range(n):
-        for j in range(i, n):
-            print(i, j, query(0, 0, n-1, i, j))
+    l = 2
+    r = 5
+    v = -10
+
+    for i in range(l, r+1):
+        val[i] += v
+
+    tree.update(l, r, v)
+
+    # Update test.
+    test(val, tree)
